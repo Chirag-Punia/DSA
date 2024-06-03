2e48c943769afeee14c16903db2a4aff47cb958b
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-03 13:20:54 +0530

    Time: 0 ms (100%), Space: 40.8 MB (91.87%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -2,24 +2,53 @@ import java.util.*;
 
 class Solution {
     public boolean check(int[] nums) {
-        int[] arr = new int[nums.length];
-        for(int i = 0; i < nums.length ; i++){
-            arr[i] = nums[i];
-        }
-        Arrays.sort(arr);
-        for(int x = 0; x< nums.length;x++){
-            boolean ans = true;
-            for(int i = 0; i< nums.length;i++){
-            if(arr[i] != nums[(i+x) % arr.length]){
-                ans = false;
-                break;
+
+        int count = 0;
+        int length = nums.length;
+        for(int i = 0; i < length-1;i++){
+            if(nums[i] > nums[i+1]){
+                //violation count
+                count++;
             }
         }
-        if(ans){
-            return ans;
-        }
-        }
-        return false;
+        //circular violation count
+        if(nums[0] < nums[length-1]) count++;
+
+        return count <= 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //TOOO SLOOWWWW
+        // int[] arr = new int[nums.length];
+        // for(int i = 0; i < nums.length ; i++){
+        //     arr[i] = nums[i];
+        // }
+        // Arrays.sort(arr);
+        // for(int x = 0; x< nums.length;x++){
+        //     boolean ans = true;
+        //     for(int i = 0; i< nums.length;i++){
+        //     if(arr[i] != nums[(i+x) % arr.length]){
+        //         ans = false;
+        //         break;
+        //     }
+        // }
+        // if(ans){
+        //     return ans;
+        // }
+        // }
+        // return false;
         
 
     }
