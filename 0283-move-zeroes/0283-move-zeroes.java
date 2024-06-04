f34385437de9ca4afebfb0baacf6f7413191a466
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-04 10:49:06 +0530

    Time: 2 ms (84.35%), Space: 45.2 MB (98.48%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,21 +1,52 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        ArrayList<Integer> arr = new ArrayList<>();
+        int j = -1;
         int n = nums.length;
-        int count = 0;
-        for(int i= 0 ; i<n; i++){
+        for(int k = 0; k < n; k++){
+            if(nums[k] == 0){
+                j = k;
+                break;
+            }
+        }
+        if(j != -1){
+            for(int i = j;i<n;i++){
             if(nums[i] != 0){
-                arr.add(nums[i]);
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+                // j will always point to zero as after swap next element will always be zero
+                j++;
             }
-            else{
-                count++;
-            }
         }
-        for(int i=0;i<n-count;i++){
-            nums[i] = arr.get(i);
         }
-        for(int i = n-count; i<n;i++){
-            nums[i] = 0;
-        }
+        
+
+     
+
+       
+
+
+
+
+        //BRUTE FORCE
+    //     ArrayList<Integer> arr = new ArrayList<>();
+    //     int n = nums.length;
+    //     int count = 0;
+    //     for(int i= 0 ; i<n; i++){
+    //         if(nums[i] != 0){
+    //             arr.add(nums[i]);
+    //         }
+    //         else{
+    //             count++;
+    //         }
+    //     }
+    //     for(int i=0;i<n-count;i++){
+    //         nums[i] = arr.get(i);
+    //     }
+    //     for(int i = n-count; i<n;i++){
+    //         nums[i] = 0;
+    //     }
+
+
     }
 }
