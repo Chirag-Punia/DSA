de20ba557d6e8060a442623d7dfc7a9cf476bcd9
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-04 16:15:45 +0530

    Time: 0 ms (100%), Space: 45.3 MB (34.17%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,16 +1,32 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int[] arr =new int[nums.length+1];
+        int n = nums.length;
+        int sum = ((n)*(n+1))/(2);
+        int temp = 0;
+        for(int i = 0; i< n ;i++){
+            temp = temp + nums[i];
+        }
+        return sum-temp;
+
+
+
+
+
+
+
+
+        //BRUTE FORCE
+        // int[] arr =new int[nums.length+1];
         
-        for(int i = 0 ; i< nums.length ; i++){
-            arr[nums[i]]++;
-        }
-        for(int i = 0; i< nums.length+1;i++){
-            if(arr[i] == 0){
-                return i;
-            }
-        }
-        return -1;
+        // for(int i = 0 ; i< nums.length ; i++){
+        //     arr[nums[i]]++;
+        // }
+        // for(int i = 0; i< nums.length+1;i++){
+        //     if(arr[i] == 0){
+        //         return i;
+        //     }
+        // }
+        // return -1;
         
     }
 }
