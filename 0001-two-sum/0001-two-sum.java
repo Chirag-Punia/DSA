efebc04f6818c4df0ef7354ab4b6ff5706ce4f76
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-05 23:55:39 +0530

    Time: 2 ms (97.73%), Space: 45.1 MB (7.38%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,23 +1,45 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer,Integer> mpp = new HashMap<>();
         int sum = 0;
-        int n = nums.length;
-        int[] ans = new int[2];
-        int left = 0;
-        int right = 1;
-        while(left < n ){
-            while(right < n){
-            sum =  nums[left] + nums[right];
-            if(sum == target){
-                ans[0] = left;
-                ans[1] = right;
+        int ans[] = new int[2];
+        for(int i = 0; i<nums.length;i++){
+            int rem = target - nums[i];
+            if(mpp.containsKey(rem)){
+                ans[0] = mpp.get(rem);
+                ans[1] = i;
                 break;
             }
-            right++;
-            }
-            left++;
-            right = left + 1;
+            mpp.put(nums[i],i);
         }
+
         return ans;
+
+
+
+
+
+
+        //BRUTE FORCE
+        // int sum = 0;
+        // int n = nums.length;
+        // int[] ans = new int[2];
+        // int left = 0;
+        // int right = 1;
+        // while(left < n ){
+        //     while(right < n){
+        //     sum =  nums[left] + nums[right];
+        //     if(sum == target){
+        //         ans[0] = left;
+        //         ans[1] = right;
+        //         break;
+        //     }
+        //     right++;
+        //     }
+        //     left++;
+        //     right = left + 1;
+        // }
+        // return ans;
     }
 }
