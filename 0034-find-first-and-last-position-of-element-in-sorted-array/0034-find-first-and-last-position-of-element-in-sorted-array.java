c2a9836a64a6a96de2db97535bbe1547f2128d4b
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-07 14:54:09 +0530

    Time: 0 ms (100%), Space: 45.8 MB (38.35%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,42 +1,65 @@
 class Solution {
-    public static int search(int[] arr , int target,int left,int right){
+    public static int search(int[] arr , int target,int left,int right ,boolean leftSearch,int ans){
         int mid = (left+right)/2;
+        int x = ans;
         if(left <= right){
             if(arr[mid] == target){
-                return mid;
+                if(leftSearch){
+                    return search(arr,target,left,mid - 1,leftSearch,mid);
+                }
+                else{
+                    return search(arr,target,mid+1,right,leftSearch,mid);
+                }
             }
             else if(arr[mid] > target){
-                return search(arr,target,left,mid-1);
+                return search(arr,target,left,mid-1,leftSearch,ans);
             }
             else{
-                return search(arr,target,mid + 1,right);
+                return search(arr,target,mid + 1,right,leftSearch,ans);
             }
         }
-        return -1;
+        return x;
         
     }
     public int[] searchRange(int[] arr, int target) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        for(int i = 0;i<arr.length;i++){
-            if(arr[i] == target){
-                ans.add(i);
-            }
-        }
-        int[] temp = new int[2];
-        if(ans.size() > 1){
-            temp[0] = ans.get(0);
-            temp[1] = ans.get(ans.size() - 1);
-        }
-        else if(ans.size() == 1){
-            temp[0] = ans.get(0);
-            temp[1] = ans.get(0);
-        }
-        else{
-            temp[0] = -1;
-            temp[1] = -1;
-        }
 
-    return temp;
+    int left = search(arr,target,0,arr.length-1,true,-1);
+    int right = search(arr,target,0,arr.length-1,false,-1);
+
+    int[] nums = {left,right};
+
+    return nums;
+
+
+
+
+
+
+
+
+
+        //BRUTE FORCE
+    //     ArrayList<Integer> ans = new ArrayList<>();
+    //     for(int i = 0;i<arr.length;i++){
+    //         if(arr[i] == target){
+    //             ans.add(i);
+    //         }
+    //     }
+    //     int[] temp = new int[2];
+    //     if(ans.size() > 1){
+    //         temp[0] = ans.get(0);
+    //         temp[1] = ans.get(ans.size() - 1);
+    //     }
+    //     else if(ans.size() == 1){
+    //         temp[0] = ans.get(0);
+    //         temp[1] = ans.get(0);
+    //     }
+    //     else{
+    //         temp[0] = -1;
+    //         temp[1] = -1;
+    //     }
+
+    // return temp;
         
     }
 }
