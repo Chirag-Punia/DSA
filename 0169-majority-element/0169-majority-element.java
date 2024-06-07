3cb505ba8d282c9640bbd11bb647d0a67fc052d4
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-07 17:03:09 +0530

    Time: 1 ms (99.79%), Space: 53 MB (42.05%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,15 +1,40 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> mpp = new HashMap<>();
-        for(int i = 0;i<nums.length;i++){
-            mpp.put(nums[i],mpp.getOrDefault(nums[i],0) + 1);
-        }
-        int[] temp = new int[1];
-        mpp.forEach((key,value) -> {
-            if(value > (nums.length/2)){
-                temp[0] = key;
+    public int majorityElement(int[] arr) {
+        int count = 0;
+        int ans = 0;
+        for(int i = 0; i<arr.length;i++){
+
+            if(count == 0){
+                ans = arr[i];
+                count = 1;
             }
-        });
-        return temp[0];
+            else if(ans == arr[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+            
+            
+        }
+        return ans;
+
+
+
+
+
+
+        //BRUTE FORCE
+        // HashMap<Integer,Integer> mpp = new HashMap<>();
+        // for(int i = 0;i<nums.length;i++){
+        //     mpp.put(nums[i],mpp.getOrDefault(nums[i],0) + 1);
+        // }
+        // int[] temp = new int[1];
+        // mpp.forEach((key,value) -> {
+        //     if(value > (nums.length/2)){
+        //         temp[0] = key;
+        //     }
+        // });
+        // return temp[0];
     }
 }
