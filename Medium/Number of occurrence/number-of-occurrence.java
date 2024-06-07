3971099744682283824a3e3ad258cb1b3d90a107
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-07 16:03:55 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number of occurrence/number-of-occurrence.java
+++ Medium/Number of occurrence/number-of-occurrence.java
@@ -36,14 +36,68 @@ public class Main {
 
 
 class Solution {
+    
+    public static int firstOccurance(int[] arr, int target,int count,int left,int right){
+        int mid = (left+right)/2;
+        int ans = count;
+        if(left <= right){
+        if(arr[mid] == target){
+            return firstOccurance(arr,target,mid,left,mid - 1);
+        }
+        else if(arr[mid] > target){
+            return firstOccurance(arr,target,count,left,mid - 1);
+        }
+        else{
+            return firstOccurance(arr,target,count,mid + 1,right);
+        }
+        }
+        return ans;
+    }
+    
+    public static int lastOccurance(int[] arr, int target,int count,int left,int right){
+        int mid = (left+right)/2;
+        int ans = count;
+        if(left <= right){
+        if(arr[mid] == target){
+            return lastOccurance(arr,target,mid,mid + 1,right);
+        }
+        else if(arr[mid] > target){
+            return lastOccurance(arr,target,count,left,mid - 1);
+        }
+        else{
+            return lastOccurance(arr,target,count,mid + 1,right);
+        }
+        }
+        return ans;
+    }
+    
+    
     int count(int[] arr, int n, int x) {
         // code here
+        
+        
+        int i = firstOccurance(arr,x,-1,0,n-1);
+        int j = lastOccurance(arr,x,-1,0,n-1);
+        if(i == -1 && j == -1){
+            return 0;
+        }
         int count = 0;
-        for(int i =0;i<n;i++){
-            if(arr[i] == x){
-                count++;
-            }
+        for(int k = i ; k<=j;k++){
+            count++;
         }
         return count;
+        
+        
+
+        
+        
+        //BRUTE FORCE
+        // int count = 0;
+        // for(int i =0;i<n;i++){
+        //     if(arr[i] == x){
+        //         count++;
+        //     }
+        // }
+        // return count;
     }
 }
