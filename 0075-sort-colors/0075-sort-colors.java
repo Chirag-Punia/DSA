7cbe8013b2d4d347533f51ec652eb85b8575b0c7
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-07 13:09:55 +0530

    Time: 0 ms (100%), Space: 41.6 MB (61.14%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,27 +1,57 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        int zeroC = 0;
-        int oneC = 0;
-        int twoC = 0;
-        for(int i = 0; i<nums.length;i++){
-            if(nums[i] == 0){
-                zeroC++;
+    public static void swap(int[] arr,int x , int y){
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+    public void sortColors(int[] arr) {
+
+        //Dutch national flag
+        
+        int low = 0,mid = 0,high = arr.length-1;
+        while(mid <= high){
+            if(arr[mid] == 0){
+                swap(arr,mid,low);
+                low++;
+                mid++;
             }
-            else if(nums[i] == 1){
-                oneC++;
+            else if(arr[mid] == 1){
+                mid++;
             }
             else{
-                twoC++;
+                swap(arr,mid,high);
+                high--;
             }
         }
-        for(int i = 0;i<zeroC;i++){
-            nums[i] = 0;
-        }
-        for(int i = zeroC;i<zeroC+oneC;i++){
-            nums[i] = 1;
-        }
-        for(int i = zeroC+oneC;i<zeroC+oneC+twoC;i++){
-            nums[i] = 2;
-        }
+
+
+
+
+        //BRUTE FORCE
+        // int zeroC = 0;
+        // int oneC = 0;
+        // int twoC = 0;
+        // for(int i = 0; i<nums.length;i++){
+        //     if(nums[i] == 0){
+        //         zeroC++;
+        //     }
+        //     else if(nums[i] == 1){
+        //         oneC++;
+        //     }
+        //     else{
+        //         twoC++;
+        //     }
+        // }
+        // for(int i = 0;i<zeroC;i++){
+        //     nums[i] = 0;
+        // }
+        // for(int i = zeroC;i<zeroC+oneC;i++){
+        //     nums[i] = 1;
+        // }
+        // for(int i = zeroC+oneC;i<zeroC+oneC+twoC;i++){
+        //     nums[i] = 2;
+        // }
+
+
     }
 }
