f513196d5875e7ccd11ddda9200575b48ea1ba8f
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-07 11:35:00 +0530

    Time: 0 ms (100%), Space: 45.6 MB (37.24%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,19 +1,39 @@
 class Solution {
-    public int search(int[] arr, int target) {
-        int right = arr.length - 1;
-        int left = 0;
-        while(left <= right){
+
+    public static int binarySearch(int[] arr , int target,int left,int right){
         int mid = (left+right)/2;
+        if(left <= right){
         if(arr[mid] == target){
             return mid;
         }
         else if(arr[mid] > target){
-            right = mid-1;
+            return binarySearch(arr,target,left,mid-1);
         }
         else{
-            left = mid+1;
+            return binarySearch(arr,target,mid + 1,right);
         }
         }
         return -1;
+    }
+    public int search(int[] arr, int target) {
+
+        return binarySearch(arr,target,0,arr.length - 1);
+
+        //ITERATIVE BINARY SEARCH
+        // int right = arr.length - 1;
+        // int left = 0;
+        // while(left <= right){
+        // int mid = (left+right)/2;
+        // if(arr[mid] == target){
+        //     return mid;
+        // }
+        // else if(arr[mid] > target){
+        //     right = mid-1;
+        // }
+        // else{
+        //     left = mid+1;
+        // }
+        // }
+        // return -1;
     }
 }
