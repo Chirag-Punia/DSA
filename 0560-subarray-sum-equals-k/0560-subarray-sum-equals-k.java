25fc0401a09d07cb97e540715c143b1a6163f5d9
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-08 13:22:56 +0530

    Time: 24 ms (74.6%), Space: 47.1 MB (9.01%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,18 +1,72 @@
 class Solution {
 
     public int subarraySum(int[] nums, int k) {
-        
+
+        HashMap<Integer,Integer> mpp = new HashMap<>();
         int sum = 0,count = 0;
-        for(int i =0;i<nums.length;i++){
-            for(int j = i;j<nums.length;j++){
-                sum = sum + nums[j];
-                if(sum == k){
-                    count++;
-                }
-                
+        for(int i = 0;i<nums.length;i++){
+            sum = sum + nums[i];
+            int rem = sum - k;
+            if(sum == k){
+                count++;
             }
-            sum = 0;
-        }
+            if(mpp.containsKey(rem)){
+                count = count + mpp.get(rem);
+            }
+
+            mpp.put(sum,mpp.getOrDefault(sum,0) + 1);
+
+        }     
         return count;
+
+
+
+
+
+
+
+
+
+
+    //     int left = 0,right = 0,sum = nums[0],count = 0;
+    //     while(left < nums.length){
+            
+    //         while(sum > k){
+    //             sum = sum - nums[left];
+    //             left++;
+    //         } 
+    //         if(sum < k){
+    //             sum = sum + nums[right];
+    //             right++;
+    //         }
+    //         if(sum == k){
+    //             count++;
+    //             right++;
+    //         }
+
+    //     }
+
+    // return count;
+
+
+
+
+
+
+
+
+        //BRUTE FORCE
+        // int sum = 0,count = 0;
+        // for(int i =0;i<nums.length;i++){
+        //     for(int j = i;j<nums.length;j++){
+        //         sum = sum + nums[j];
+        //         if(sum == k){
+        //             count++;
+        //         }
+                
+        //     }
+        //     sum = 0;
+        // }
+        // return count;
     }
 }
