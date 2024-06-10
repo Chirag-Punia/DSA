1c602371c05299bfde2f353bf1e88960478f68b9
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-11 01:04:51 +0530

    Time: 2 ms (21.64%), Space: 45.6 MB (20.4%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -5,18 +5,22 @@ class Solution {
         int n = matrix[0].length;
         int col0 = 1;
         int row0 = 1;
+
+        // set condition for row0
         for (int i = 1; i < n; i++) {
             if (matrix[0][i] == 0) {
                 row0 = 0;
                 break;
             }
         }
+        // set condition for col0
         for (int i = 0; i < m; i++) {
             if (matrix[i][0] == 0) {
                 col0 = 0;
                 break;
             }
         }
+        // set condition for internal matrix
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
@@ -25,6 +29,7 @@ class Solution {
                 }
             }
         }
+        // Take care of internal matrix coloums wise
         for (int i = 1; i < m; i++) {
             if (matrix[i][0] == 0) {
                 for (int j = 1; j < n; j++) {
@@ -32,6 +37,8 @@ class Solution {
                 }
             }
         }
+
+        // Take care of internal matrix row wise
         for (int i = 1; i < n; i++) {
             if (matrix[0][i] == 0) {
                 for (int j = 1; j < m; j++) {
@@ -39,7 +46,9 @@ class Solution {
                 }
             }
         }
-        if(matrix[0][0] == 0){
+
+        // Take care of both rows and coloumns
+        if (matrix[0][0] == 0) {
             for (int i = 0; i < n; i++) {
 
                 matrix[0][i] = 0;
@@ -51,6 +60,7 @@ class Solution {
 
             }
         }
+        // take care of rows
         if (row0 == 0) {
             for (int i = 0; i < n; i++) {
 
@@ -58,6 +68,7 @@ class Solution {
 
             }
         }
+        // take care for coloumns
         if (col0 == 0) {
             for (int i = 0; i < m; i++) {
 
@@ -65,8 +76,7 @@ class Solution {
 
             }
         }
-
-
+        // BRUTE FORCE
         // ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
         // ArrayList<Integer> temp = new ArrayList<>();
         // for(int i = 0;i<m;i++){
