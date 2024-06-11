c009b1ca00fa2166f2b67271e101cabb39280bdf
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-11 12:56:20 +0530

    Time: 0 ms (100%), Space: 50.4 MB (29.27%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,10 +1,42 @@
 class Solution {
-    public int singleNonDuplicate(int[] nums) {
-        int ans = 0;
-        for(int i = 0 ;i<nums.length;i++){
-            ans = nums[i] ^ ans;
+    public static int binarySearch(int[] arr, int left, int right) {
+        int mid = (left + right) / 2;
+
+        if (arr[mid] != arr[mid + 1] && arr[mid] != arr[mid - 1]) {
+            return arr[mid];
+        } else if (mid % 2 == 0) {
+            if (arr[mid] == arr[mid + 1]) {
+                return binarySearch(arr, mid, right);
+            } else {
+                return binarySearch(arr, left, mid);
+            }
+
+        } else {
+            if (arr[mid] == arr[mid - 1]) {
+                return binarySearch(arr, mid, right);
+            } else {
+                return binarySearch(arr, left, mid);
+            }
         }
-        return ans;
-          
+    }
+
+    public int singleNonDuplicate(int[] arr) {
+
+        if (arr.length == 1) {
+            return arr[0];
+        } else if (arr[arr.length - 1] != arr[arr.length - 2]) {
+            return arr[arr.length - 1];
+        } else if (arr[0] != arr[1]) {
+            return arr[0];
+        }
+        return binarySearch(arr, 0, arr.length - 1);
+
+        // BRUTE FORCCE
+        // int ans = 0;
+        // for(int i = 0 ;i<nums.length;i++){
+        // ans = nums[i] ^ ans;
+        // }
+        // return ans;
+
     }
 }
