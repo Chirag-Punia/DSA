23847a8ddfaf9665481916eb6b671d15fbccea4e
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-12 17:15:46 +0530

    Time: 0 ms (100%), Space: 41.9 MB (93.99%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,11 +1,23 @@
 class Solution {
-    public int findPeakElement(int[] arr) {
+    public static int binarySearch(int[] arr, int left, int right) {
 
-        for (int i = 1; i < arr.length - 1; i++) {
-            if (arr[i] > arr[i + 1] && arr[i] > arr[i - 1]) {
-                return i;
+        int mid = (right + left) / 2;
+        if (left <= right) {
+            if (arr[mid] >= arr[mid + 1] && arr[mid - 1] <= arr[mid]) {
+                return mid;
+            } else if (arr[mid] < arr[mid + 1] && arr[mid - 1] < arr[mid]) {
+                return binarySearch(arr, mid + 1, right);
+            } else if (arr[mid - 1] > arr[mid] && arr[mid + 1] < arr[mid]) {
+                return binarySearch(arr, left, mid - 1);
+            }
+            else{
+                return binarySearch(arr, left, mid - 1);
             }
         }
+        return mid;
+    }
+
+    public int findPeakElement(int[] arr) {
         if (arr.length > 1) {
             if (arr[0] > arr[1]) {
                 return 0;
@@ -16,6 +28,14 @@ class Solution {
             return 0;
         }
 
-        return 0;
+        return binarySearch(arr, 1, arr.length - 2);
+        // BRUTE FORCE
+        // for (int i = 1; i < arr.length - 1; i++) {
+        // if (arr[i] > arr[i + 1] && arr[i] > arr[i - 1]) {
+        // return i;
+        // }
+        // }
+
+        // return 0;
     }
 }
