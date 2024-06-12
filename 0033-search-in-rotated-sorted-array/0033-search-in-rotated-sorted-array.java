c6ea7aa9428f435e8616a6faf05f7589cd3e0110
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-12 11:01:17 +0530

    Time: 0 ms (100%), Space: 42.2 MB (22.41%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,12 +1,44 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        
-        
-        for(int i = 0;i<nums.length;i++){
-            if(nums[i] == target){
-                return i;
+
+    public static int binarySearch(int[] arr,int target,int left,int right){
+        int mid  = (left+right)/2;
+        if(left <= right){
+            if(arr[mid] == target){
+            return mid;
+        }
+        else if(arr[left] <= arr[mid]){
+            if(arr[left] <= target && target <= arr[mid]){
+                return binarySearch(arr,target,left,mid-1);
+            }
+            else{
+                return binarySearch(arr,target,mid + 1,right);
             }
         }
-        return -1;
+        else{
+            if(target <= arr[right] && target >= arr[mid]){
+                return binarySearch(arr,target,mid + 1,right);
+            }
+            else{
+                return binarySearch(arr,target,left,mid-1);
+            }
+            
+        }
+        }
+
+        return - 1;
+        
+
+    }
+    public int search(int[] nums, int target) {
+        
+        return binarySearch(nums,target,0,nums.length-1);
+
+        //BRUTE FORCE
+        // for(int i = 0;i<nums.length;i++){
+        //     if(nums[i] == target){
+        //         return i;
+        //     }
+        // }
+        // return -1;
     }
 }
