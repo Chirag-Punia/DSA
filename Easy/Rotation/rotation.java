31104cba4b5c0b1b8107135086eef427feeded14
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-12 16:45:28 +0530

    Updated Solution - GfG to GitHub

--- Easy/Rotation/rotation.java
+++ Easy/Rotation/rotation.java
@@ -30,15 +30,36 @@ class GFG {
 // User function Template for Java
 
 class Solution {
-    int findKRotation(int arr[], int n) {
-        // code here
-        int count = 0;
-        for(int i =0;i<n-1;i++){
-            if(arr[i+1] < arr[i]){
-                count = i+1;
-                break;
+    
+    public static int binarySearch(int[] arr,int left,int right){
+        int mid = (right+left) / 2;
+        if(left <= right){
+            if(left == right){
+                return left;
+            }
+            if(arr[mid] <= arr[right]){
+                return binarySearch(arr,left,mid);
+            }
+            else{
+                return binarySearch(arr,mid + 1,right);
             }
         }
-        return count;
+        return left;
+    }
+    
+    int findKRotation(int arr[], int n) {
+        // code here
+        
+        return binarySearch(arr,0,arr.length-1);
+        
+        //BRUTE FORCE
+        // int count = 0;
+        // for(int i =0;i<n-1;i++){
+        //     if(arr[i+1] < arr[i]){
+        //         count = i+1;
+        //         break;
+        //     }
+        // }
+        // return count;
     }
 }
