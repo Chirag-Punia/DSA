db0cab353c686e2eeecb566dfd7d31dca7cd859f
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-13 21:21:27 +0530

    Time: 0 ms (100%), Space: 42.7 MB (48.1%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
@@ -1,25 +1,52 @@
 class Solution {
-    public int findKthPositive(int[] arr, int k) {
-        int temp = 0;
-        int max = Integer.MIN_VALUE;
-        for(int i= 0;i<arr.length;i++){
-            max = Math.max(max,arr[i]);
-        }
-        for(int i = 1; i<=max+k;i++){
-            boolean x = false;
-            for(int j = 0;j<arr.length;j++){
+    public static boolean isPresent(int[] arr,int i){
+        boolean x = false;
+        for(int j = 0;j<arr.length;j++){
                 if(i == arr[j]){
                     x = true;
                     break;
                 }
             }
-            if(!x){
-                temp++;
-                if(temp == k){
-                    return i;
-                }
+        return x;
+    }
+
+    public int findKthPositive(int[] arr, int k) {
+
+        //BRUTE FORCE
+        for(int i = 0; i < arr.length;i++){
+            if(arr[i] <= k){
+                k++;
+            }
+            else{
+                break;
             }
         }
-        return -1;
+        return k;
+
+
+
+
+        //JAMA HI BRUTE FORCE
+        // int temp = 0;
+        // int max = Integer.MIN_VALUE;
+        // for(int i= 0;i<arr.length;i++){
+        //     max = Math.max(max,arr[i]);
+        // }
+        // for(int i = 1; i<=max+k;i++){
+        //     boolean x = false;
+        //     for(int j = 0;j<arr.length;j++){
+        //         if(i == arr[j]){
+        //             x = true;
+        //             break;
+        //         }
+        //     }
+        //     if(!x){
+        //         temp++;
+        //         if(temp == k){
+        //             return i;
+        //         }
+        //     }
+        // }
+        // return -1;
     }
 }
