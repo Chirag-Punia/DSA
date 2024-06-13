78a5aad025a49b0d4103164fe2f3384eb8456615
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-13 16:51:04 +0530

    Time: 9 ms (5.06%), Space: 42.3 MB (94.29%) - LeetHub

---
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int temp = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i= 0;i<arr.length;i++){
+            max = Math.max(max,arr[i]);
+        }
+        for(int i = 1; i<=max+k;i++){
+            boolean x = false;
+            for(int j = 0;j<arr.length;j++){
+                if(i == arr[j]){
+                    x = true;
+                    break;
+                }
+            }
+            if(!x){
+                temp++;
+                if(temp == k){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
