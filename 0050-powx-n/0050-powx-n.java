2570cdd7898b043a0c2869bf7a190ad07451a6fd
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-15 02:22:41 +0530

    Time: 0 ms (100%), Space: 42.3 MB (34.98%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,13 +1,50 @@
 class Solution {
-    public static double p(double x,int n){
-        if(n == 0){
+    public static double p(double x, int n) {
+        if (n == 0) {
             return 1;
         }
-        return x*p(x,n-1);
+        return x * p(x, n - 1);
     }
-     
-    
+
     public double myPow(double x, int n) {
-    return Math.pow(x,n);
-}
+
+
+        //FAST EXPONENTION
+        if(n < 0){
+            n = -n;
+            x = 1 / x;
+        }
+        double ans = x;
+        double temp = 1;
+        while(n != 0){
+            if((n & 1) == 1){
+            temp =  ans * temp;
+        }
+            ans = ans * ans;
+            n >>>= 1;
+        }
+        return temp;
+
+
+        //BRUTE FORCE
+        // int t = n;
+        // if (n < 0) {
+        //     n = n * -1;
+        // }
+
+        // double shortPower = p(x, n / 2);
+
+        // if (n % 2 == 0) {
+        //     if(t < 0){
+        //         return 1/(shortPower * shortPower);
+        //     }
+        //     return shortPower * shortPower;
+        // }
+        // else{
+        //     if(t < 0){
+        //         return 1/(x * shortPower * shortPower);
+        //     }
+        //     return x * shortPower * shortPower;
+        // }
+    }
 }
