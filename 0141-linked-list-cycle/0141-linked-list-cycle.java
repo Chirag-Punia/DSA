838661841d654e8eface06afc605fa7e850bd072
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-18 00:27:45 +0530

    Time: 0 ms (100%), Space: 44.3 MB (40.55%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -12,19 +12,38 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
 
-    HashMap<ListNode,Integer> mpp = new HashMap<>();
-    if(head == null){
+    //TORTOISE
+    ListNode slow = head;
+    ListNode fast = head;
+    if(head == null || head.next == null || head.next.next == null){
         return false;
     }
-    ListNode current = head;
-    while(current.next != null){
-        if(mpp.containsKey(current)){
+    while(fast != null && fast.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow == fast){
             return true;
         }
-        mpp.put(current,1);
-        current = current.next;
     }
     return false;
+
+
+
+
+    // BRUTE FORCE
+    // HashMap<ListNode,Integer> mpp = new HashMap<>();
+    // if(head == null){
+    //     return false;
+    // }
+    // ListNode current = head;
+    // while(current.next != null){
+    //     if(mpp.containsKey(current)){
+    //         return true;
+    //     }
+    //     mpp.put(current,1);
+    //     current = current.next;
+    // }
+    // return false;
 
     
     }
