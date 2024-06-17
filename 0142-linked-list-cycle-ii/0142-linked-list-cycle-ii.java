43ac3316870fa661f3d5d2786f64f71ffd408f18
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-18 01:07:22 +0530

    Time: 1 ms (28.43%), Space: 44.1 MB (81.84%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -1,30 +1,50 @@
 /**
  * Definition for singly-linked list.
  * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
  * }
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        
-    HashMap<ListNode,Integer> mpp = new HashMap<>();
-    if(head == null){
-        return null;
-    }
-    ListNode current = head;
-    while(current.next != null){
-        if(mpp.containsKey(current)){
-            return current;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        if (head == null || head.next == null || head.next.next == null) {
+            return null;
         }
-        mpp.put(current,1);
-        current = current.next;
-    }
-    return null;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+
+        // BRUTE FORCE
+        // HashMap<ListNode,Integer> mpp = new HashMap<>();
+        // if(head == null){
+        // return null;
+        // }
+        // ListNode current = head;
+        // while(current.next != null){
+        // if(mpp.containsKey(current)){
+        // return current;
+        // }
+        // mpp.put(current,1);
+        // current = current.next;
+        // }
+        // return null;
 
     }
 }
