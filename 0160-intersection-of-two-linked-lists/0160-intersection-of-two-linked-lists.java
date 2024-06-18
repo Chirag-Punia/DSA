f80a64c74ab8b8b2efbc717cce933e0265aff4fa
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-19 00:26:08 +0530

    Time: 1 ms (99.85%), Space: 48.4 MB (60.6%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -10,20 +10,54 @@
  * }
  */
 public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        HashMap<ListNode,Boolean> mpp = new HashMap<>();
-        ListNode a = headA;
-        ListNode b = headB;
-        while(a != null){
-            mpp.put(a,true);
-            a = a.next;
+    public static ListNode collision(ListNode a , ListNode b,int point){
+        while(point > 0){
+            b = b.next;
+            point--;
         }
-        while(b != null){
-            if(mpp.getOrDefault(b,false)) return b;
+        while(a != null && b != null){
+            if(a == b){
+                return a;
+            }
+            a = a.next;
             b = b.next;
         }
         return null;
 
+    }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 
+        ListNode a = headA;
+        ListNode b = headB;
+        int n1 = 0;
+        int n2 = 0;
+        while(a != null){
+            n1++;
+            a = a.next;
+        }
+        while(b != null){
+            n2++;
+            b = b.next;
+        }
+        a = headA;
+        b = headB;
+        if(n2 >= n1){
+            return collision(a,b,n2-n1);
+        }
+        else{
+            return collision(b,a,n1-n2);
+        }
+        // HashMap<ListNode,Boolean> mpp = new HashMap<>();
+        // ListNode a = headA;
+        // ListNode b = headB;
+        // while(a != null){
+        //     mpp.put(a,true);
+        //     a = a.next;
+        // }
+        // while(b != null){
+        //     if(mpp.getOrDefault(b,false)) return b;
+        //     b = b.next;
+        // }
+        // return null;
     }
 }
