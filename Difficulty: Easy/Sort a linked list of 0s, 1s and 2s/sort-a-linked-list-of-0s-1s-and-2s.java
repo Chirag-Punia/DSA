ea6165ae1d5d78436acb3df453d7faa5a2255071
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-18 22:46:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.java
@@ -0,0 +1,103 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int key) {
+        data = key;
+        next = null;
+    }
+}
+
+public class Driverclass {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine().trim());
+
+        while (t-- > 0) {
+            int n = Integer.parseInt(in.readLine().trim());
+            String s[] = in.readLine().trim().split(" ");
+            Node head = new Node(Integer.parseInt(s[0]));
+            Node tail = head;
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(s[i]));
+                tail = tail.next;
+            }
+
+            head = new Solution().segregate(head);
+            printList(head, out);
+            out.println();
+        }
+        out.close();
+    }
+
+    public static void printList(Node head, PrintWriter out) {
+        if (head == null) return;
+
+        Node temp = head;
+        while (temp != null) {
+            out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+*/
+class Solution {
+    // Function to sort a linked list of 0s, 1s and 2s.
+    static Node segregate(Node head) {
+        // add your code here
+        if(head == null || head.next == null){
+            return head;
+        }
+        Node curr = head;
+        int zeroCnt = 0, oneCnt = 0, twoCnt = 0;
+        while(curr != null){
+            if(curr.data == 0) zeroCnt++;
+            else if(curr.data == 1) oneCnt++;
+            else if(curr.data == 2) twoCnt++;
+            curr = curr.next;
+        }
+        curr = head;
+        while(zeroCnt > 0){
+            curr.data = 0;
+            curr = curr.next;
+            zeroCnt--;
+        }
+        while(oneCnt > 0){
+            curr.data = 1;
+            curr = curr.next;
+            oneCnt--;
+        }
+        while(twoCnt > 0){
+            curr.data = 2;
+            curr = curr.next;
+            twoCnt--;
+        }
+        return head;
+        
+        
+    }
+}
