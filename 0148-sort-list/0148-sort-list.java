fb3bd051d5555a0b7072c93c2804198fea6160f4
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-18 22:24:20 +0530

    Time: 12 ms (32.76%), Space: 57 MB (27.66%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -9,27 +9,87 @@
  * }
  */
 class Solution {
+    public static ListNode mid(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    public static ListNode merge(ListNode newLeft,ListNode newRight){
+        ListNode mergeLL = new ListNode(-1);
+        ListNode temp = mergeLL;
+        while(newLeft != null && newRight != null){
+            if(newLeft.val < newRight.val){
+                temp.next = newLeft;
+                newLeft = newLeft.next;
+            }
+            else{
+                temp.next = newRight;
+                newRight = newRight.next;
+            }
+            temp = temp.next;
+        }
+        while(newLeft != null){
+            temp.next = newLeft;
+            newLeft = newLeft.next;
+            temp = temp.next;
+        }
+        while(newRight != null){
+            temp.next = newRight;
+            newRight = newRight.next;
+            temp = temp.next;
+        }
+        return mergeLL.next;
+    }
+
+    public static ListNode mergeSort(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        //breaking into half
+        ListNode mid = mid(head);
+        ListNode rightHead = mid.next;
+        mid.next = null;
+
+        ListNode newLeft = mergeSort(head);
+        ListNode newRight = mergeSort(rightHead);
+        return merge(newLeft,newRight);
+
+    }
+
 
     public ListNode sortList(ListNode head) {
         if(head == null || head.next == null){
             return head;
         }
+        ListNode ans  = mergeSort(head);
+        return ans;
 
-        ArrayList<Integer> arr = new ArrayList<>();
-        ListNode temp = head;
-        while(temp != null){
-            arr.add(temp.val);
-            temp = temp.next;
-        }
-        Collections.sort(arr);
-        temp = head;
-        int i = 0;
-        while(temp != null){
-            temp.val = arr.get(i);
-            i++;
-            temp = temp.next;
-        }
-        return head;
+
+
+
+        //VERY MUCH BRUTE FORCE
+        // ArrayList<Integer> arr = new ArrayList<>();
+        // ListNode temp = head;
+        // while(temp != null){
+        //     arr.add(temp.val);
+        //     temp = temp.next;
+        // }
+        // Collections.sort(arr);
+        // temp = head;
+        // int i = 0;
+        // while(temp != null){
+        //     temp.val = arr.get(i);
+        //     i++;
+        //     temp = temp.next;
+        // }
+        // return head;
 
 
         //BUBBLE SORT
