e5400585af9d5f0a93843fdb5fcd3933dc899e25
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-20 16:20:19 +0530

    Time: 3 ms (12.95%), Space: 41.3 MB (71.16%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,41 @@
+public class Solution {
+    public static boolean isValid(String s) {
+        if (s.length() <= 1) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        stack.push(s.charAt(0));
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (!stack.isEmpty()) {
+                if (stack.peek() == '[' && s.charAt(i + 1) == ']') {
+                    stack.pop();
+                } else if (stack.peek() == '{' && s.charAt(i + 1) == '}') {
+                    stack.pop();
+                } else if (stack.peek() == '(' && s.charAt(i + 1) == ')') {
+                    stack.pop();
+                } else {
+                    stack.push(s.charAt(i+1));
+                }
+            }
+            else{
+                stack.push(s.charAt(i+1));
+            }
+
+        }
+        return stack.isEmpty();
+
+        // BRUTE FORCE
+        // while (true) {
+        // if (s.contains("()")) {
+        // s = s.replace("()", "");
+        // } else if (s.contains("{}")) {
+        // s = s.replace("{}", "");
+        // } else if (s.contains("[]")) {
+        // s = s.replace("[]", "");
+        // } else {
+
+        // return s.isEmpty();
+        // }
+        // }
+    }
+}
