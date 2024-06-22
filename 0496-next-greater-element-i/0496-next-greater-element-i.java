8b604be9854f2e6043459829390da739840b382d
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-22 12:31:39 +0530

    Time: 5 ms (21.07%), Space: 43.1 MB (83.57%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,23 +1,55 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] ans = new int[nums1.length];
+
+
+        //OPTIMAL
+        int[] ans = new int[nums2.length];
+        int[] ans2 = new int[nums1.length];
         Stack<Integer> s = new Stack<>();
+        for(int i = nums2.length - 1;i>=0;i--){
+            int curr = nums2[i];
+            while(!s.isEmpty() && curr >= nums2[s.peek()]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                ans[i] = -1;
+            }
+            else{
+                ans[i] = nums2[s.peek()];
+            }
+            s.push(i);
+        }
         for(int i = 0;i<nums1.length;i++){
             int curr = nums1[i];
-            boolean isFound = false;
-            for(int j = 0;j<nums2.length;j++){
-                if(nums2[j] == curr) isFound = true;
-                if(nums2[j] > curr && isFound){
-                    ans[i] = nums2[j];
-                    break;
+            for(int j = 0; j<nums2.length;j++){
+                if(curr == nums2[j]){
+                    ans2[i] = ans[j];
                 }
-                else{
-                    ans[i] = -1;
-                }
-
             }
         }
-        return ans;
+        return ans2;
+
+
+
+        //BRUTE FORCE
+        // int[] ans = new int[nums1.length];
+        // Stack<Integer> s = new Stack<>();
+        // for(int i = 0;i<nums1.length;i++){
+        //     int curr = nums1[i];
+        //     boolean isFound = false;
+        //     for(int j = 0;j<nums2.length;j++){
+        //         if(nums2[j] == curr) isFound = true;
+        //         if(nums2[j] > curr && isFound){
+        //             ans[i] = nums2[j];
+        //             break;
+        //         }
+        //         else{
+        //             ans[i] = -1;
+        //         }
+
+        //     }
+        // }
+        // return ans;
 
     }
 }
