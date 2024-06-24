dac6dd49b83b722c1129e0f43525a41b318aa56e
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-25 00:49:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Smallest/kth-smallest.java
+++ Difficulty: Medium/Kth Smallest/kth-smallest.java
@@ -34,20 +34,55 @@ class GFG {
 //User function Template for Java
 
 class Solution{
-    public static int kthSmallest(int[] arr, int l, int r, int k) 
+    public static void heapify(int i, int[] arr, int n) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int largest = i;
+
+        if (left < n && arr[left] < arr[largest]) largest = left;
+        if (right < n && arr[right] < arr[largest]) largest = right;
+
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+            heapify(largest, arr, n);
+        }
+    }
+    public static int kthSmallest(int[] nums, int l, int r, int k) 
     { 
         //Your code here
-        PriorityQueue<Integer> p = new PriorityQueue<>();
-        while(l <= r){
-            p.add(arr[l]);
-            l++;
-        }
+        int n = nums.length;
         int temp = -1;
-        while(k > 0){
-            temp = p.remove();
-            k--;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(i, nums, n);
         }
+
+        for (int i = n - 1; i >= n - k; i--) {
+
+            temp = nums[0];
+            nums[0] = nums[i];
+            nums[i] = temp;
+            heapify(0, nums, i);
+
+        }
+
         return temp;
+        
+        
+        //BRUTE FORCE
+        // PriorityQueue<Integer> p = new PriorityQueue<>();
+        // while(l <= r){
+        //     p.add(arr[l]);
+        //     l++;
+        // }
+        // int temp = -1;
+        // while(k > 0){
+        //     temp = p.remove();
+        //     k--;
+        // }
+        // return temp;
         
     } 
 }
