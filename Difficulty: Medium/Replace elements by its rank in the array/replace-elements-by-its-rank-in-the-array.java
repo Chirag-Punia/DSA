d4bf4d36da4345347968b97122b4cc03513a5e70
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-25 23:58:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Replace elements by its rank in the array/replace-elements-by-its-rank-in-the-array.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    while (T-- > 0) {
+      int n = sc.nextInt();
+      int a[] = new int[n];
+      for(int i=0;i<n;i++){
+        a[i]=sc.nextInt();
+      }
+      Solution obj = new Solution();
+      int ans[] = obj.replaceWithRank(a,n);
+      for(int e : ans)
+      System.out.print(e+" ");
+      System.out.println();
+    }
+  }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int[] replaceWithRank(int arr[], int N) {
+     // code here
+     int[] ans = new int[N];
+     PriorityQueue<Integer> pq = new PriorityQueue<>();
+     HashMap<Integer,Integer> mpp = new HashMap<>();
+     for(int i = 0;i<N;i++){
+         pq.add(arr[i]);
+     }
+     int rank = 1; // Start with rank 1
+        while (!pq.isEmpty()) {
+            int element = pq.poll();
+            if (!mpp.containsKey(element)) {
+                mpp.put(element, rank++);
+            }
+        }
+     for(int i = 0;i<N;i++){
+         ans[i] = mpp.get(arr[i]);
+     }
+     return ans;
+    
+    
+  }
+}
+     
