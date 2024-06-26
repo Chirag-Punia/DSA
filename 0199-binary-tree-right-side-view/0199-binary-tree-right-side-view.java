e9a274bd491c430498cb691a0da553b28f1f30ab
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-26 14:50:30 +0530

    Time: 0 ms (100%), Space: 42 MB (33.28%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -22,30 +22,41 @@ class Solution {
             this.row = row;
         }
     }
+    public static void right(TreeNode root,int level,List<Integer> arr){
+        if(root == null) return;
+        if(level == arr.size()) arr.add(root.val);
+        right(root.right,level + 1,arr);
+        right(root.left,level + 1,arr);
+    }
     public List<Integer> rightSideView(TreeNode root) {
 
-        Queue<nodeInfo> q = new LinkedList<>();
-        TreeMap<Integer,List<Integer>> mpp = new TreeMap<>();
-        List<Integer> ans = new ArrayList<>();
-        if(root == null){
-            return ans;
-        }
-        q.offer(new nodeInfo(root,0));
-        while(!q.isEmpty()){
-            nodeInfo c = q.poll();
-            mpp.computeIfAbsent(c.row,k -> new ArrayList<>()).add(c.node.val);
-            if(c.node.left != null){
-                q.offer(new nodeInfo(c.node.left,c.row + 1));
-            }
-            if(c.node.right != null){
-                q.offer(new nodeInfo(c.node.right,c.row + 1));
-            }
-        }
+        List<Integer> arr = new ArrayList<>();
+        right(root,0,arr);
+        return arr;
+
+        //BRUTE FORCE
+        // Queue<nodeInfo> q = new LinkedList<>();
+        // TreeMap<Integer,List<Integer>> mpp = new TreeMap<>();
+        // List<Integer> ans = new ArrayList<>();
+        // if(root == null){
+        //     return ans;
+        // }
+        // q.offer(new nodeInfo(root,0));
+        // while(!q.isEmpty()){
+        //     nodeInfo c = q.poll();
+        //     mpp.computeIfAbsent(c.row,k -> new ArrayList<>()).add(c.node.val);
+        //     if(c.node.left != null){
+        //         q.offer(new nodeInfo(c.node.left,c.row + 1));
+        //     }
+        //     if(c.node.right != null){
+        //         q.offer(new nodeInfo(c.node.right,c.row + 1));
+        //     }
+        // }
         
-        for(Map.Entry<Integer,List<Integer>> entry : mpp.entrySet()){
-            List<Integer> x = entry.getValue();
-            ans.add(x.get(x.size() - 1));
-        }
-        return ans;
+        // for(Map.Entry<Integer,List<Integer>> entry : mpp.entrySet()){
+        //     List<Integer> x = entry.getValue();
+        //     ans.add(x.get(x.size() - 1));
+        // }
+        // return ans;
     }
 }
