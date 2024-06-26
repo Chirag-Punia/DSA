5420d290c07599897cab411808a05e7151b17f64
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-26 17:11:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.java
+++ Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.java
@@ -34,19 +34,37 @@ class GFG{
 
 class Solution
 {
+    public static class Pair implements Comparable<Pair>{
+        int i;
+        int[] arr;
+        Pair(int i,int[] arr){
+            this.i = i;
+            this.arr = arr;
+        }
+        @Override
+        public  int compareTo(Pair other){
+            return Integer.compare(this.arr[this.i],other.arr[other.i]);
+        }
+    }
     //Function to merge k sorted arrays.
     public static ArrayList<Integer> mergeKArrays(int[][] arr,int k) 
     {
         // Write your code here.
-        PriorityQueue<Integer> q = new PriorityQueue<>();
+        PriorityQueue<Pair> q = new PriorityQueue<>();
         for(int i = 0;i<k;i++){
-            for(int j = 0;j<k;j++){
-                q.add(arr[i][j]);
-            }
+            Pair temp = new Pair(0,arr[i]);
+            q.offer(temp);
         }
         ArrayList<Integer> ans = new ArrayList<>();
-        for(int i = 0;i<k*k;i++){
-            ans.add(q.remove());
+        while(!q.isEmpty()){
+            Pair t = q.remove();
+            ans.add(t.arr[t.i]);
+            int e = t.i + 1;
+            if(e < k) {
+                Pair x = new Pair(e,t.arr);
+                q.offer(x);
+            }
+            
         }
         return ans;
         
