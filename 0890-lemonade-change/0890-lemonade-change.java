25e37514c5bcd00c434b96f6a358cff7ee3b9b39
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-27 17:28:18 +0530

    Time: 2 ms (94.96%), Space: 55.8 MB (40.86%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.java
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -1,24 +1,18 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        int n = bills.length;
-        ArrayList<Integer> change = new ArrayList<>();
-        for(int i = 0;i<n;i++){
-            int c = bills[i];
-            if(c == 5){change.add(c);}
-            else{
-                int x = c - 5;
-                Collections.sort(change);
-                for(int j = change.size() - 1;j>=0;j--){
-                        if(x >= change.get(j)){
-                            x = x - change.get(j);
-                            change.remove(j);
-                        }
-                        if(x == 0){
-                            break;
-                        }
-                }
-                if(x != 0) return false;
-                change.add(c);
+        int count5 = 0,count10 = 0;
+        for(int bill : bills){
+            if(bill == 5)count5++;
+            else if(bill == 10){
+                if(count5 == 0) return false;
+                count5--;
+                count10++;
+            }
+            else if(bill == 20){
+                if(count5 == 0 && count10 == 0)return false;
+                if(count5 >= 1 && count10 >= 1){count5--;count10--;}
+                else if(count5 >= 3)count5 -= 3;
+                else return false;
             }
         }
         return true;
