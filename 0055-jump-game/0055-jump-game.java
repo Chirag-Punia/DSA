4fa87b6ef5271bdb59776b9dd542c90e8523a80e
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-28 12:28:40 +0530

    Time: 2 ms (79.85%), Space: 45.4 MB (74.41%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,10 +1,14 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int n = nums.length,maxi = 0;
+        int n = nums.length;
+        int maxi = 0;
+        if(n == 1) return true;
         for(int i = 0;i<n;i++){
+            int jump = i + nums[i];
             if(i > maxi) return false;
-            maxi = Math.max(maxi,i+nums[i]);
+            if(jump > maxi) maxi = jump;
+            if(maxi >= n-1) return true;
         }
-        return true;
+        return false;
     }
 }
