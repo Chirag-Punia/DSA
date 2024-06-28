40981f3b84b2de911f472e37b37a32f89d2054b2
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-06-28 12:28:00 +0530

    Time: 3 ms (53%), Space: 45.7 MB (32.89%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,22 +1,4 @@
 class Solution {
-    public static boolean ans;
-    public Solution(){
-        ans = false;
-    }
-    public static int ij(int[] nums,int curr,int end){
-        int idx = curr;
-        int max = nums[curr];
-        for(int i = curr;i<=end;i++){
-            if(nums[i] > max && i<nums.length){
-                max = nums[i];
-                idx = i;
-            }
-            if(i >= nums.length){
-                ans = true;
-            }
-        }
-        return idx;
-    }
     public boolean canJump(int[] nums) {
         int n = nums.length,maxi = 0;
         for(int i = 0;i<n;i++){
