04572da65de95e823a57828659f10e97382ec5e6
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-07-02 14:44:01 +0530

    Time: 2 ms (12.03%), Space: 44.7 MB (72.19%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -26,20 +26,37 @@ class Solution {
             if(j + 1 < m && grid[i][j+1] == t && !mpp.getOrDefault(new Pair(i,j+1),false)) {grid[i][j+1] = c;q.offer(new Pair(i,j+1));mpp.put(new Pair(i,j+1),true);}
             if(j - 1 >= 0 && grid[i][j-1] == t && !mpp.getOrDefault(new Pair(i,j-1),false)) {grid[i][j-1] = c;q.offer(new Pair(i,j-1));mpp.put(new Pair(i,j-1),true);}
     }
+    public static void dfs(int[][] grid,int i,int j,int n,int m,int c,int t){
+        grid[i][j] = c;
+        int[] x = {1,-1,0,0};
+        int[] y = {0,0,1,-1};
+        for(int k = 0;k < 4;k++){
+            Pair tmp = new Pair(i+x[k],j+y[k]);
+            if(i + x[k] < n && i + x[k] >= 0 && j + y[k] >=0 && j + y[k] < m && grid[i+x[k]][j + y[k]] == t && !mpp.getOrDefault(tmp,false)) {
+                grid[i+x[k]][j + y[k]] = c;
+                mpp.put(tmp,true);
+                dfs(grid,i + x[k],j + y[k],n,m,c,t);
+            }
+        }
+    }
     public static int[][] floodFill(int[][] grid, int sr, int sc, int color) {
         int n = grid.length;
         int m = grid[0].length;
         mpp = new HashMap<>();
         int target = grid[sr][sc];
-        grid[sr][sc] = color;
-        Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(sr,sc));
-        mpp.put(new Pair(sr,sc),true);
-        while(!q.isEmpty()){
-            Pair c = q.poll();
+        dfs(grid,sr,sc,n,m,color,target);
+        // Queue<Pair> q = new LinkedList<>();
+        // q.offer(new Pair(sr,sc));
+        // mpp.put(new Pair(sr,sc),true);
+        // while(!q.isEmpty()){
+        //     Pair c = q.poll();
 
-            makeIt2(grid,c.i,c.j,n,m,q,color,target);
-        }
+        //     makeIt2(grid,c.i,c.j,n,m,q,color,target);
+        // }
+        // return grid;
+
+
         return grid;
+
     }
 }
