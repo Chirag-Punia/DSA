177fdd53fc17497342320ccad0b13982372d32c0
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-07-02 11:35:01 +0530

    Time: 3 ms (18.35%), Space: 42.6 MB (19.19%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -9,24 +9,20 @@ class Solution {
             this.min=min;
         }
     }
+    public static int cnt2 = 0;
     public static HashMap<Pair,Boolean> mpp = new HashMap<>();
-    public static boolean ans;
-    public static int minutes;
-    public static int count(int[][] grid){
-        int cnt1 = 0;
-        for(int i = 0;i<grid.length;i++){
-            for(int j=0;j<grid[i].length;j++){
-                if(grid[i][j] == 1) cnt1++;
-            }
-        }
-        return cnt1;
-    }
-    public static void makeIt2(int[][] grid,int i,int j,int n,int m,int min,Queue<Pair> q){
-            if(i + 1 < n && grid[i+1][j] == 1 && !mpp.getOrDefault(new Pair(i+1,j,min),false)) {grid[i+1][j] = 2;q.offer(new Pair(i+1,j,min+1));}
-            if(i - 1 >= 0 && grid[i-1][j] == 1 && !mpp.getOrDefault(new Pair(i-1,j,min),false)) {grid[i-1][j] = 2;q.offer(new Pair(i-1,j,min+1));}
-            if(j + 1 < m && grid[i][j+1] == 1 && !mpp.getOrDefault(new Pair(i,j+1,min),false)) {grid[i][j+1] = 2;q.offer(new Pair(i,j+1,min+1));}
-            if(j - 1 >= 0 && grid[i][j-1] == 1 && !mpp.getOrDefault(new Pair(i,j-1,min),false)) {grid[i][j-1] = 2;q.offer(new Pair(i,j-1,min+1));}
-    }
+    // public static boolean ans;
+    // public static int minutes;
+    // public static int count(int[][] grid){
+    //     int cnt1 = 0;
+    //     for(int i = 0;i<grid.length;i++){
+    //         for(int j=0;j<grid[i].length;j++){
+    //             if(grid[i][j] == 1) cnt1++;
+    //         }
+    //     }
+    //     return cnt1;
+    // }
+    
     // public static void gridT(int[][] grid,int n,int m){
     //     boolean changed;
     //     do {
@@ -62,16 +58,26 @@ class Solution {
 
     //     if (!ans) minutes = -1;
     // }
+
+    public static void makeIt2(int[][] grid,int i,int j,int n,int m,int min,Queue<Pair> q){
+            if(i + 1 < n && grid[i+1][j] == 1 && !mpp.getOrDefault(new Pair(i+1,j,min),false)) {grid[i+1][j] = 2;q.offer(new Pair(i+1,j,min+1));++cnt2;}
+            if(i - 1 >= 0 && grid[i-1][j] == 1 && !mpp.getOrDefault(new Pair(i-1,j,min),false)) {grid[i-1][j] = 2;q.offer(new Pair(i-1,j,min+1));++cnt2;}
+            if(j + 1 < m && grid[i][j+1] == 1 && !mpp.getOrDefault(new Pair(i,j+1,min),false)) {grid[i][j+1] = 2;q.offer(new Pair(i,j+1,min+1));++cnt2;}
+            if(j - 1 >= 0 && grid[i][j-1] == 1 && !mpp.getOrDefault(new Pair(i,j-1,min),false)) {grid[i][j-1] = 2;q.offer(new Pair(i,j-1,min+1));++cnt2;}
+    }
     public int orangesRotting(int[][] grid) {
 
         int n = grid.length;
         int m = grid[0].length;
+        int cnt1 = 0;
+        cnt2 = 0;
 
         Queue<Pair> q = new LinkedList<>();
         int ans = 0;
         for(int i = 0;i<grid.length;i++){
             for(int j=0;j<grid[i].length;j++){
                 if(grid[i][j] == 2){Pair t = new Pair(i,j,0);q.offer(t);mpp.put(t,true);}
+                if(grid[i][j] == 1)cnt1++;
             }
         }
         while(!q.isEmpty()){
@@ -79,7 +85,7 @@ class Solution {
             ans = Math.max(ans,c.min);
             makeIt2(grid,c.i,c.j,n,m,c.min,q);
         }
-        if(count(grid) > 0) return -1;
+        if(cnt1 != cnt2) return -1;
         return ans;
     }
 }
