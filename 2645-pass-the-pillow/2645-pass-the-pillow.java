f50233a73a3915bfccba418e96ee8aee2cacd1bf
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-07-06 12:10:01 +0530

    Time: 0 ms (100%), Space: 40.1 MB (74.29%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.java
+++ 2645-pass-the-pillow/2645-pass-the-pillow.java
@@ -1,14 +1,20 @@
 class Solution {
     public int passThePillow(int n, int time) {
-        if(time < n) return time+1;
-        int i = 1;
-        int drn = 1;
-        while(time > 0){
-            if(i >= n) drn = -1;
-            if(i == 1) drn = 1;
-            i += drn;
-            time--;
+
+        if ((time / (n - 1)) % 2 == 0) {
+            return (time % (n - 1)) + 1;
+        } else {
+            return n - (time % (n - 1));
         }
-        return i;
+
+        // int i = 1;
+        // int drn = 1;
+        // while(time > 0){
+        // if(i >= n) drn = -1;
+        // if(i == 1) drn = 1;
+        // i += drn;
+        // time--;
+        // }
+        // return i;
     }
 }
