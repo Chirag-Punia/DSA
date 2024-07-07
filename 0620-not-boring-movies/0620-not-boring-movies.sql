64352263e6fa0676eff0d248a584aca6d2117d60
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-07-07 22:03:45 +0530

    Time: 232 ms (68.34%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
 select *
 from Cinema c
-where c.id % 2 <> 0 and c.description <> 'boring'
+where c.id % 2 <> 0 
+and lower(c.description) not like '%boring%'
 order by c.rating desc;
