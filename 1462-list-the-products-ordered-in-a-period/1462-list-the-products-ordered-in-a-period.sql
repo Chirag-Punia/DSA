61419c166fb6107fbd94478c0fec1dff85d26c2c
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-07-10 01:38:36 +0530

    Time: 1240 ms (36.09%), Space: 0B (100%) - LeetHub

--- 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
@@ -1,14 +1,24 @@
-# Write your MySQL query statement below
-with resOrder as(
-select *, sum(unit) as total
-from Orders 
-where month(order_date) = 2 and year(order_date) = 2020
-group by product_id
-having total >= 100)
+WITH resOrder AS (
+    SELECT
+        product_id,
+        SUM(unit) AS total
+    FROM
+        Orders
+    WHERE
+        MONTH(order_date) = 2
+        AND YEAR(order_date) = 2020
+    GROUP BY
+        product_id
+    HAVING
+        total >= 100
+)
 
-
-select p.product_name,r.total as unit
-from Products p
-left join resOrder r
-on p.product_id = r.product_id
-where r.total is not null;
+SELECT
+    p.product_name,
+    COALESCE(r.total, 0) AS unit
+FROM
+    Products p
+LEFT JOIN
+    resOrder r ON p.product_id = r.product_id
+WHERE
+    r.total IS NOT NULL;
