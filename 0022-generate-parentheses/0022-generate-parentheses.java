b31ad68d33b872c4d393383480ef280550eb84b5
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-07-14 10:50:25 +0530

    Time: 1 ms (84.45%), Space: 43.4 MB (21.21%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<String> ans;
+    public void tmp(int o,int c, int n,String s){
+        if(s.length() == 2*n){ans.add(s);return;}
+        if(o < n)tmp(o+1,c,n,s+'(');
+        if(o>c)tmp(o,c+1,n,s+')');
+    }
+    public List<String> generateParenthesis(int n) {
+        ans = new ArrayList<>();
+        tmp(0,0,n,"");
+        return ans;
+    }
+}
