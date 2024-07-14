2033e665007d1a5b8f674bfc52330eef82dcb4a8
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-07-14 13:01:24 +0530

    Time: 0 ms (100%), Space: 42.7 MB (35.01%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,23 +1,21 @@
 import java.util.ArrayList;
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-
-        List<List<Integer>> arrMain = new ArrayList<>();
-        int n = 1 << nums.length;
-        for(int i = 0;i<n;i++){
-            int count = -1;
-            int j = i;
-            List<Integer> arr = new ArrayList<>();
-            while(j > 0){
-                count++;
-                if((j & 1) == 1){
-                    arr.add(nums[count]);
-                }
-                
-                j >>= 1;
-            }
-            arrMain.add(arr);
+    public List<List<Integer>> ans;
+    public List<Integer> arr;
+    public void tmp(int i,int[] nums){
+        if(i >= nums.length){
+            ans.add(new ArrayList<>(arr));
+            return;
         }
-        return arrMain;
+        arr.add(nums[i]);
+        tmp(i+1,nums);
+        arr.remove(arr.size() - 1);
+        tmp(i+1,nums);
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        ans = new ArrayList<>();
+        arr = new ArrayList<>();
+        tmp(0,nums);
+        return ans;
     }
 }
