f13e970dfa5ba9660122434952b5456fb6283d30
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-07-15 13:33:59 +0530

    Time: 5 ms (37.6%), Space: 42.1 MB (34.85%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<String> ans;
+    public String mpp[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    public void temp(List<Integer> digits,String tmp,int i){
+        if(i == digits.size()){
+            ans.add(tmp);
+            return;
+        }
+        for(int j = 0;j<mpp[digits.get(i)].length();j++){
+            temp(digits,tmp+mpp[digits.get(i)].charAt(j),i+1);
+        }
+    }
+    public static List<Integer> convertStringToDigits(String str) {
+        List<Integer> digits = new ArrayList<>();
+        for (char ch : str.toCharArray()) {
+            digits.add(Character.getNumericValue(ch));
+        }
+        return digits;
+    }
+    public List<String> letterCombinations(String d) {
+        ans = new ArrayList<>();
+        if (d == null || d.length() == 0) {
+            return ans;
+        }
+        List<Integer> digits = convertStringToDigits(d);
+        temp(digits,"",0);
+        return ans;
+    }
+}
