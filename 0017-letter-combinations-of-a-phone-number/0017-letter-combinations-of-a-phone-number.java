769fc61ddaf9e715f7fbe731ec6c24da677e0efb
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-07-15 14:18:12 +0530

    Time: 5 ms (37.6%), Space: 42.3 MB (14.64%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,12 +1,25 @@
 class Solution {
     public List<String> ans;
-    public void temp(String digits,String tmp,int i,HashMap<String,String> mpp){
+    private static final HashMap<Character, String> mpp = new HashMap<>();
+    static {
+        mpp.put('2', "abc");
+        mpp.put('3', "def");
+        mpp.put('4', "ghi");
+        mpp.put('5', "jkl");
+        mpp.put('6', "mno");
+        mpp.put('7', "pqrs");
+        mpp.put('8', "tuv");
+        mpp.put('9', "wxyz");
+    }
+    public void temp(String digits,String tmp,int i){
         if(i == digits.length()){
             ans.add(tmp);
             return;
         }
-        for(int j = 0;j<mpp.get(Character.toString(digits.charAt(i))).length();j++){
-            temp(digits,tmp+mpp.get(Character.toString(digits.charAt(i))).charAt(j),i+1,mpp);
+        String letters = mpp.get(digits.charAt(i));
+        for(int j = 0;j<letters.length();j++){
+            String newStr = tmp+letters.charAt(j);
+            temp(digits,newStr,i+1);
         }
     }
     public List<String> letterCombinations(String digits) {
@@ -14,16 +27,7 @@ class Solution {
         if (digits == null || digits.length() == 0) {
             return ans;
         }
-        HashMap<String,String> mpp = new HashMap<>();
-        mpp.put("2","abc");
-        mpp.put("3","def");
-        mpp.put("4","ghi");
-        mpp.put("5","jkl");
-        mpp.put("6","mno");
-        mpp.put("7","pqrs");
-        mpp.put("8","tuv");
-        mpp.put("9","wxyz");
-        temp(digits,"",0,mpp);
+        temp(digits,"",0);
         return ans;
     }
 }
