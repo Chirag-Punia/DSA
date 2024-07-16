0aa29dc1d599b72b4e6d427fff3109d454ec3076
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-07-16 23:56:11 +0530

    Time: 51 ms (24.9%), Space: 47.7 MB (25.03%) - LeetHub

---
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.java
@@ -0,0 +1,24 @@
+class Solution {
+      public int func(int[] nums, int goal) {
+        int left = 0,right=0,cnt=0,s=0,n=nums.length;
+        HashMap<Integer,Integer> mpp = new HashMap<>();
+        if (goal < 0)
+            return 0;
+        while (right < n) {
+            int curr = nums[right];
+            mpp.put(curr,mpp.getOrDefault(curr,0)+1);
+            
+            while (mpp.size() > goal) {
+                mpp.put(nums[left],mpp.getOrDefault(nums[left],0)-1);
+                if(mpp.get(nums[left]) == 0)mpp.remove(nums[left]);
+                left++;
+            }
+            cnt += right - left + 1;
+            right++;
+        }
+    return cnt;
+    }
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return func(nums,k) - func(nums,k-1);
+    }
+}
