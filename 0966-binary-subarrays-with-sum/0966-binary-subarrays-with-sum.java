1f6298a42fcf37ee740bd410790a7d19474dc65d
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-07-16 15:15:36 +0530

    Time: 1 ms (100%), Space: 47.7 MB (50.4%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,14 +1,39 @@
 class Solution {
-    public int numSubarraysWithSum(int[] nums, int goal) {
+    public int func(int[] nums, int goal) {
+        int left = 0;
+        int right = 0;
         int cnt = 0;
+        int s = 0;
         int n = nums.length;
-        for(int i = 0;i<n;i++){
-            int s = 0;
-            for(int j = i;j<n;j++){
-                s+=nums[j];
-                if(s == goal)cnt++;
+        if (goal < 0)
+            return 0;
+        while (right < n) {
+            s += nums[right];
+            while (s > goal) {
+                s -= nums[left];
+                left++;
             }
+            cnt += right - left + 1;
+            right++;
         }
-        return cnt;
+    return cnt;
     }
+
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        // int cnt = 0;
+        // int n = nums.length;
+        // for (int i = 0; i < n; i++) {
+        //     int s = 0;
+        //     for (int j = i; j < n; j++) {
+        //         s += nums[j];
+        //         if (s == goal)
+        //             cnt++;
+        //     }
+        // }
+        // return cnt;
+        int a = func(nums,goal);
+        int b = func(nums,goal-1);
+        return a - b;
+    }
+
 }
