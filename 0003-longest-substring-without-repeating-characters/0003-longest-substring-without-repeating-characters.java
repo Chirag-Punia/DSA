29f76534ec79220963230a55a2177c68f5bcf49a
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-07-16 10:38:37 +0530

    Time: 6 ms (68.31%), Space: 44.5 MB (47.48%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,25 +1,45 @@
 class Solution {
-    public int count(String s){
-        HashMap<Character,Integer> mpp = new HashMap<>();
-        for(int i = 0;i< s.length();i++){
+    public int count(String s) {
+        HashMap<Character, Integer> mpp = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
             char curr = s.charAt(i);
-            mpp.put(curr,mpp.getOrDefault(curr,0)+1);
-            if(mpp.getOrDefault(curr,0) > 1) return -1;
+            mpp.put(curr, mpp.getOrDefault(curr, 0) + 1);
+            if (mpp.getOrDefault(curr, 0) > 1)
+                return -1;
         }
         return s.length();
     }
+
     public int lengthOfLongestSubstring(String s) {
-        if(s == null || s.length() == 0)return 0;
-        int ans = 0;
-        for(int i = 0;i<s.length();i++){
-            HashSet<Character> seen = new HashSet<>();
-            for(int j = i;j<s.length();j++){
-                char currChar = s.charAt(j);
-                if(seen.contains(currChar))break;
-                seen.add(currChar);
-                ans = Math.max(ans,j - i + 1);
+        if (s == null || s.length() == 0)
+            return 0;
+        HashMap<Character, Integer> mpp = new HashMap<>();
+        int left = 0;
+        int right = 0;
+        int n = s.length();
+        int maxLen = 0;
+        while (left < n && right < n) {
+            char rightChar = s.charAt(right);
+            if(mpp.containsKey(rightChar) && mpp.get(rightChar) >= left) {
+                left = mpp.get(rightChar) + 1;
             }
+            mpp.put(rightChar, right);
+            maxLen = Math.max(maxLen, right - left + 1);
+            right++;
         }
-        return ans;
+        return maxLen;
+
+        // BRUTE FORCE
+        // int ans = 0;
+        // for(int i = 0;i<s.length();i++){
+        // HashSet<Character> seen = new HashSet<>();
+        // for(int j = i;j<s.length();j++){
+        // char currChar = s.charAt(j);
+        // if(seen.contains(currChar))break;
+        // seen.add(currChar);
+        // ans = Math.max(ans,j - i + 1);
+        // }
+        // }
+        // return ans;
     }
 }
