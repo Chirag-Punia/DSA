a2a32d641c97b37cd65ba922add40a4e94b7a1f1
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-07-16 13:27:05 +0530

    Time: 23 ms (29.76%), Space: 44.8 MB (11.43%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int n = s.length();
+        int ans = 0;
+        
+        // for (int i = 0; i < n; i++) {
+        //     HashMap<Character, Integer> mpp = new HashMap<>();
+        //     int maxFreq = 0;
+            
+        //     for (int j = i; j < n; j++) {
+        //         char curr = s.charAt(j);
+        //         mpp.put(curr, mpp.getOrDefault(curr, 0) + 1);
+        //         maxFreq = Math.max(maxFreq, mpp.get(curr));
+                
+        //         int change = (j - i + 1) - maxFreq;
+        //         if (change <= k) {
+        //             ans = Math.max(ans, j - i + 1);
+        //         } else {
+        //             break;
+        //         }
+        //     }
+        // }
+
+        int left = 0;
+        int right = 0;
+        int maxFreq = 0;
+        HashMap<Character, Integer> mpp = new HashMap<>();
+        while(right < n){
+            char curr = s.charAt(right);
+            mpp.put(curr, mpp.getOrDefault(curr, 0) + 1);
+            maxFreq = Math.max(maxFreq, mpp.get(curr));
+
+            int change = (right - left + 1) - maxFreq;
+            if (change <= k) {
+                    ans = Math.max(ans, right - left + 1);
+            } else {
+                while(((right - left + 1) - maxFreq) > k){
+                    char tmp = s.charAt(left);
+                    mpp.put(tmp, mpp.getOrDefault(tmp, 0) - 1);
+                    if(mpp.get(tmp) == 0)mpp.remove(tmp);
+                    left++;
+                }
+            }
+            right++;
+        }
+        
+        return ans;
+    }
+}
