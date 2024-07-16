51e42009479948ec3e8a73b8a3b65ec797e15d91
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-07-16 12:32:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Fruit Into Baskets/fruit-into-baskets.java
+++ Difficulty: Medium/Fruit Into Baskets/fruit-into-baskets.java
@@ -30,17 +30,32 @@ class GFG {
 
 class Solution {
     public static int totalFruits(int N, int[] fruits) {
-        HashSet<Integer> set;
+        HashMap<Integer,Integer> mpp = new HashMap<>();
         int n = fruits.length;
         int ans = 0;
-        for(int i = 0;i< n; i++){
-            set = new HashSet<>();
-            for(int j = i;j<n;j++){
-                set.add(fruits[j]);
-                if(set.size() > 2)break;
-                ans = Math.max(ans,j - i + 1);
+        int left = 0;
+        int right = 0;
+        while(right < n){
+            mpp.put(fruits[right],mpp.getOrDefault(fruits[right],0)+1);
+            while(mpp.size() > 2){
+                int curr = fruits[left];
+                mpp.put(curr,mpp.get(curr) - 1);
+                if(mpp.get(curr) == 0)mpp.remove(curr);
+                left++;
             }
+            ans = Math.max(ans,right - left + 1);
+            right++;
+
         }
         return ans;
+        // for(int i = 0;i< n; i++){
+        //     set = new HashSet<>();
+        //     for(int j = i;j<n;j++){
+        //         set.add(fruits[j]);
+        //         if(set.size() > 2)break;
+        //         ans = Math.max(ans,j - i + 1);
+        //     }
+        // }
+        // return ans;
     }
 }
