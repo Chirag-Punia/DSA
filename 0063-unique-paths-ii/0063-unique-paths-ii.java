937032b84fcd5eb9a5eade84f56736e514998112
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-07-20 16:07:32 +0530

    Time: 0 ms (100%), Space: 41.7 MB (29.53%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[][] dp;
+
+    public int tmp(int i, int j, int[][] o) {
+        if (i == o.length - 1 && j == o[0].length - 1)
+             return o[i][j] == 1 ? 0 : 1;
+        else if (i >= o.length || j >= o[0].length || o[i][j] == 1)
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        int l = tmp(i, j + 1,o);
+        int r = tmp(i + 1, j,o);
+        return dp[i][j] = l + r;
+    }
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        dp = new int[obstacleGrid.length][obstacleGrid[0].length];
+        for (int i = 0; i < obstacleGrid.length; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return tmp(0, 0, obstacleGrid);
+    }
+}
