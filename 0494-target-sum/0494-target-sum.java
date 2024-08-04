cf192c686e42eb648848567e85377c4402a7aa0b
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-08-04 07:50:00 +0530

    Time: 101 ms (45.85%), Space: 45.8 MB (5.25%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,14 +1,22 @@
 class Solution {
-    public int s;
-    public int tmp(int[] nums,int target,int i,int currSum){
-        if(i<0 && target == currSum)return 1;
-        if(i < 0)return 0;
-        int pos = tmp(nums,target,i-1,currSum+nums[i]);
-        int neg = tmp(nums,target,i-1,currSum-nums[i]);
-        return pos+neg;
+    public HashMap<String,Integer> dp;
+
+    public int tmp(int[] nums, int target, int i, int currSum) {
+        String key = i+","+currSum;
+        if (dp.containsKey(key))
+            return dp.get(key);
+        if (i < 0 && target == currSum)
+            return 1;
+        if (i < 0)
+            return 0;
+        int pos = tmp(nums, target, i - 1, currSum + nums[i]);
+        int neg = tmp(nums, target, i - 1, currSum - nums[i]);
+        dp.put(key,pos+neg);
+        return dp.get(key);
     }
+
     public int findTargetSumWays(int[] nums, int target) {
-        s = 0;
-        return tmp(nums,target,nums.length-1,0);
+        dp = new HashMap<>();
+        return tmp(nums, target, nums.length - 1, 0);
     }
 }
