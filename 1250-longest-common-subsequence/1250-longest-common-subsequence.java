737ad0a33bdaf0cd4c13d6077599c7c2268e58ff
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-08-05 17:40:03 +0530

    Time: 465 ms (5.06%), Space: 222.3 MB (5.01%) - LeetHub

---
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -0,0 +1,22 @@
+class Solution {
+    HashMap<String, Integer> memo;
+
+    int tmp(String s1, String s2, int i, int j) {
+        String key = i + "" + j;
+        if (i >= s1.length() || j >= s2.length())
+            return 0;
+        if (memo.containsKey(key))
+            return memo.get(key);
+        if (s1.charAt(i) == s2.charAt(j))
+            return 1 + tmp(s1, s2, i + 1, j + 1);
+        memo.put(key, Math.max(tmp(s1, s2, i + 1, j), tmp(s1, s2, i, j + 1)));
+        return memo.get(key);
+    }
+
+    public int longestCommonSubsequence(String s1, String s2) {
+        memo = new HashMap<>();
+        if (s1.equals(s2))
+            return s1.length();
+        return tmp(s1, s2, 0, 0);
+    }
+}
