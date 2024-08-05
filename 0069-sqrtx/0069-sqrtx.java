18a11d630173d6ae78da454c06b6d705b5dfede1
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-08-05 20:08:22 +0530

    Time: 1 ms (85.94%), Space: 40.6 MB (62.56%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,10 +1,20 @@
 class Solution {
-    public int mySqrt(int x) {
-        if(x==1 || x == 2)return 1;
-        for(long i = 1;i<x;i++){
-            if(i * i == x)return (int)i;
-            if(i * i > x)return (int)i-1;
+    public  int binarySearch(int x,long ans,long left,long right){
+        long mid = (right+left) / 2;
+        if(left <= right){
+            if(mid*mid <= x){
+                return binarySearch(x,mid,mid + 1,right);
+            }
+            else{
+                return binarySearch(x,ans,left,mid-1);
+            }
         }
-        return 0;
+        return (int)ans;
+    }
+    public int mySqrt(int x) {
+        if(x == 0)return 0;
+        if(x==1 || x == 2)return 1;
+        
+        return binarySearch(x,1,1,(long)x);
     }
 }
