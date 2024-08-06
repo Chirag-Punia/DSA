595ee40fd944b335006132c9d430cf61943aa906
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-08-06 19:25:10 +0530

    Time: 1 ms (90.1%), Space: 46 MB (22.82%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,10 +1,10 @@
 class Solution {
-    int profit;
     int[][] dp;
     int tmp(int[] arr,int buy,int i){
         if(i >= arr.length){
             return 0;
         }
+        int profit = 0;
         if(dp[i][buy] != -1)return dp[i][buy];
         if(buy == 1){
             profit = Math.max((-arr[i] + tmp(arr,0,i+1)),tmp(arr,1,i+1));
@@ -15,12 +15,27 @@ class Solution {
 
         return dp[i][buy] = profit;
     }
-
+    private int tabulation(int[] arr,int n){
+        int[][] dp = new int[n+1][2];
+        int profit = 0;
+        dp[n][0] = dp[n][1] = 0;
+        for(int i = n-1;i>=0;i--){
+            for(int j = 1;j>=0;j--){
+                if(j == 1){
+                    dp[i][j] = Math.max((-arr[i] + dp[i+1][0]),dp[i+1][1]);
+                }
+                else{
+                    dp[i][j] = Math.max((arr[i] + dp[i+1][1]),dp[i+1][0]);
+                }
+            }
+            
+        }
+        return dp[0][1];
+    }
     public int maxProfit(int[] arr) {
         
-        dp = new int[arr.length+1][2];
-        for(int[] r : dp)Arrays.fill(r,-1);
-        profit = Integer.MIN_VALUE;
-        return tmp(arr,1,0);
+        // dp = new int[arr.length+1][2];
+        // for(int[] r : dp)Arrays.fill(r,-1);
+        return tabulation(arr,arr.length);
     }
 }
