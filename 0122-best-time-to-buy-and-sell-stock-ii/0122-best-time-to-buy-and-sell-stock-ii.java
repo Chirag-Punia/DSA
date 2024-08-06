d18ee1078b64289aeb58066a50fab65c4d17d89c
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-08-06 18:58:20 +0530

    Time: 20 ms (6.67%), Space: 59.5 MB (5.46%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    int profit;
+    Map<String,Integer> memo;
+    int[][] dp;
+    int tmp(int[] arr,int buy,int i){
+        String key = buy+""+i;
+        if(i >= arr.length){
+            return 0;
+        }
+        if(memo.containsKey(key))return memo.get(key);
+        if(buy == 1){
+            profit = Math.max((-arr[i] + tmp(arr,0,i+1)),tmp(arr,1,i+1));
+        }
+        else{
+            profit = Math.max((arr[i] + tmp(arr,1,i+1)) , tmp(arr,0,i+1));
+        }
+        memo.put(key,profit);
+        return profit;
+    }
+
+    public int maxProfit(int[] arr) {
+        memo = new HashMap<>();
+        profit = Integer.MIN_VALUE;
+        return tmp(arr,1,0);
+    }
+}
