67b9deff0952e0b6abd12ec65897fe49a8a08141
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-08-07 18:10:30 +0530

    Time: 4 ms (99.67%), Space: 55.3 MB (45.96%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -46,7 +46,7 @@ class Solution {
         int[] prev = new int[2];
         ahead[0] = ahead[1] = 0;
         for (int i = n - 1; i >= 0; i--) {
-            for (int j = 1; j >= 0; j--) {
+            for (int j = 0; j <= 1; j++) {
           
                     if (j == 1) {
                         prev[j] = Math.max((-arr[i] + ahead[0]), ahead[1]);
@@ -59,7 +59,7 @@ class Solution {
                 
             
         }
-        return ahead[1];
+        return prev[1];
     }
 
     public int maxProfit(int[] prices, int fee) {
