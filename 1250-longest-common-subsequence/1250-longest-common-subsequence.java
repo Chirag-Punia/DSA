74949ce410ff439e89998267e5e11b88c8163f37
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-08-11 15:08:59 +0530

    Time: 16 ms (94.14%), Space: 48.5 MB (92.17%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -13,10 +13,26 @@ class Solution {
         return memo.get(key);
     }
 
+    int tabulation(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n+1][m+1];
+        for (int i = n-1; i >= 0; i--) {
+            for (int j = m-1; j >= 0; j--) {
+                if (s1.charAt(i) == s2.charAt(j))
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+
     public int longestCommonSubsequence(String s1, String s2) {
         memo = new HashMap<>();
         if (s1.equals(s2))
             return s1.length();
-        return tmp(s1, s2, 0, 0);
+        return tabulation(s1, s2);
     }
 }
