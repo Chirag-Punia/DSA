9ca0290bcd1fcfd83f50bfbebf76f66bc4dac5f9
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-08-14 20:23:12 +0530

    Time: 0 ms (100%), Space: 44.5 MB (27.49%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -13,22 +13,61 @@ class Node {
 }
 */
 
+// class Solution {
+//     public Node copyRandomList(Node head) {
+//         Node temp = head;
+//         HashMap<Node,Node> mpp = new HashMap<>();
+//         while(temp != null){
+//             mpp.put(temp,new Node(temp.val));
+//             temp = temp.next;
+//         }
+//         temp = head;
+//         while(temp != null){
+//             Node copyNode = mpp.get(temp);
+//             copyNode.next = mpp.get(temp.next);
+//             copyNode.random = mpp.get(temp.random);
+//             temp = temp.next;
+//         }
+//         return mpp.get(head);
+
+//     }
+// }
+
 class Solution {
     public Node copyRandomList(Node head) {
-        Node temp = head;
-        HashMap<Node,Node> mpp = new HashMap<>();
-        while(temp != null){
-            mpp.put(temp,new Node(temp.val));
-            temp = temp.next;
-        }
-        temp = head;
-        while(temp != null){
-            Node copyNode = mpp.get(temp);
-            copyNode.next = mpp.get(temp.next);
-            copyNode.random = mpp.get(temp.random);
-            temp = temp.next;
-        }
-        return mpp.get(head);
+        if (head == null) return null;
 
+        // Step 1: Create new nodes and insert them next to original nodes
+        Node temp = head;
+        while (temp != null) {
+            Node newNode = new Node(temp.val);
+            newNode.next = temp.next;
+            temp.next = newNode;
+            temp = newNode.next;
+        }
+
+        // Step 2: Assign the random pointers
+        temp = head;
+        while (temp != null) {
+            if (temp.random != null) {
+                temp.next.random = temp.random.next;
+            }
+            temp = temp.next.next;
+        }
+
+        // Step 3: Separate the original list from the copied list
+        temp = head;
+        Node dummy = head.next;
+        Node res = dummy;
+        while (temp != null) {
+            temp.next = temp.next.next;
+            if (dummy.next != null) {
+                dummy.next = dummy.next.next;
+            }
+            temp = temp.next;
+            dummy = dummy.next;
+        }
+
+        return res;
     }
 }
