50f466b69b40f1c3d56aa3eb4393d809292d6c7a
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-08-14 15:56:11 +0530

    Time: 0 ms (100%), Space: 41.7 MB (89.73%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,7 +1,19 @@
 class Solution {
+    public boolean binarySearch(int[] a, int left, int right, int target) {
+        if (left > right)
+            return false;
+        int mid = left + (right - left) / 2;
+        if (a[mid] == target)
+            return true;
+        else if (a[mid] < target)
+            return binarySearch(a, mid + 1, right, target);
+        else
+            return binarySearch(a, left, mid - 1, target);
+    }
+
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int[] a : matrix)
-            for(int b : a)if(b == target)return true;
+        for (int[] a : matrix)
+            if(binarySearch(a, 0, a.length - 1, target))return true;
         return false;
     }
 }
