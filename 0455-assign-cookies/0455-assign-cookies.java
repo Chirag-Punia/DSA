d05a725260f4b7e8e4c0e67eb1ab15aab02d8c14
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-08-15 16:07:38 +0530

    Time: 8 ms (98.34%), Space: 45.2 MB (54.26%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -3,11 +3,18 @@ class Solution {
 
         Arrays.sort(g);
         Arrays.sort(s);
-        int count = 0,i=0,j=0;
-        
-        while(j < s.length && i < g.length){
-            if(g[i] > s[j]){j++;continue;}
-            if(s[j] >= g[i]) {count++;j++;}
+
+        int count = 0, i = 0, j = 0;
+
+        while (j < s.length && i < g.length) {
+            if (g[i] > s[j]) {
+                j++;
+                continue;
+            }
+            if (s[j] >= g[i]) {
+                count++;
+                j++;
+            }
             i++;
         }
         return count;
