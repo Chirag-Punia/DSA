b92f8d82af93a57567cfc3c600af771f7cb10c9b
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-08-16 22:56:27 +0530

    Time: 2 ms (82.1%), Space: 44.6 MB (70.84%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,17 +1,36 @@
+// class Solution {
+//     public int maxProduct(int[] nums) {
+//         long ans = Long.MIN_VALUE;
+//         for(int i = 0;i<nums.length;i++){     
+//             long prod = nums[i];
+//             ans = Math.max(ans,prod);
+//             for(int j = i + 1;j<nums.length;j++){
+//                 prod = prod * nums[j];
+//                 if (prod > Integer.MAX_VALUE || prod < Integer.MIN_VALUE) {
+//                     break;
+//                 }
+//                 ans = Math.max(ans,prod);
+//             }
+//         }
+//         return (int)ans;
+//     }
+
+// }
+
 class Solution {
     public int maxProduct(int[] nums) {
-        long ans = Long.MIN_VALUE;
-        for(int i = 0;i<nums.length;i++){     
-            long prod = nums[i];
-            ans = Math.max(ans,prod);
-            for(int j = i + 1;j<nums.length;j++){
-                prod = prod * nums[j];
-                if (prod > Integer.MAX_VALUE || prod < Integer.MIN_VALUE) {
-                    break;
-                }
-                ans = Math.max(ans,prod);
-            }
+        double suffix = 1, prefix = 1;
+        int maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (suffix == 0)
+                suffix = 1;
+            if (prefix == 0)
+                prefix = 1;
+            prefix *= (double) nums[i];
+            suffix *= (double) nums[nums.length - i - 1];
+            maxi = (int) Math.max(maxi, Math.max(prefix, suffix));
         }
-        return (int)ans;
+        return maxi;
     }
+
 }
