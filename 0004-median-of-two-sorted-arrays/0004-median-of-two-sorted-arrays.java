3fbcb0b6527868da28f8a8deb1e69da27a449f10
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-08-17 15:31:30 +0530

    Time: 2 ms (63.54%), Space: 45.6 MB (88.13%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,14 +1,50 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        List<Double> arr = new ArrayList<>();
-        int n = nums1.length,m = nums2.length;
-        for(int i : nums1)
-            arr.add((double)i);
-        for(int i : nums2)
-            arr.add((double)i);
-        Collections.sort(arr);
-        if(arr.size() % 2 == 0)
-            return (arr.get((n+m) / 2) + arr.get(((n+m) / 2) - 1))/2;
-        return arr.get((n+m) / 2);
+        int i = 0,j=0,cnt=0,n = nums1.length,m=nums2.length,ele1 = 0,ele2 = 0;
+        while(i < n && j < m){
+            if(nums1[i] >= nums2[j]){
+                if(cnt == ((n + m)/2 - 1)){
+                    ele1 = nums2[j];
+                }
+                else if(cnt == ((n + m)/2)){
+                    ele2 = nums2[j];
+                }
+                j++;
+                cnt++;
+            }
+            else{
+                if(cnt == ((n + m)/2 - 1)){
+                    ele1 = nums1[i];
+                }
+                else if(cnt == ((n + m)/2)){
+                    ele2 = nums1[i];
+                }
+                i++;
+                cnt++;
+            }
+        }
+        while(i < n){
+            if(cnt == ((n + m)/2 - 1)){
+                    ele1 = nums1[i];
+                }
+                else if(cnt == ((n + m)/2)){
+                    ele2 = nums1[i];
+                }
+                i++;
+                cnt++;
+        }
+        while(j < m){
+            if(cnt == ((n + m)/2 - 1)){
+                ele1 = nums2[j];
+            }
+            else if(cnt == ((n + m)/2)){
+                ele2 = nums2[j];
+            }
+            j++;
+            cnt++;
+        }
+
+        if((n+m) % 2 != 0)return (double)ele2;
+        return (double)(((double)ele1 + (double)ele2)/ 2);
     }
 }
