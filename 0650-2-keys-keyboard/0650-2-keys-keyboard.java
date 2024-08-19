837f16ac5692047399ccc994cdaec05335ced5a0
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-08-19 11:06:06 +0530

    Time: 24 ms (20.95%), Space: 54.9 MB (13.64%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -1,15 +1,20 @@
 class Solution {
+    int[][] dp;
+    int tmp(int count,int paste,int n){
+        if(count == n)return 0;
+        if(count > n)return 10000;
+        
+        if(dp[count][paste] != -1)return dp[count][paste];
+        int pastee = 1 + tmp(count + paste,paste,n);
+        int copyAndPaste = 2 + tmp(count + count,count,n);
+
+        return dp[count][paste] = Math.min(pastee,copyAndPaste);
+    }
     public int minSteps(int n) {
-        if (n == 1) return 0;
-        
-        int steps = 0;
-        for (int i = 2; i <= n; i++) {
-            while (n % i == 0) {
-                steps += i;
-                n /= i;
-            }
-        }
-        
-        return steps;
+        dp = new int[n+1][n+1];
+        for(int[] row : dp)
+            Arrays.fill(row,-1);
+        if(n == 1)return 0;
+        return 1+ tmp(1,1,n);
     }
 }
