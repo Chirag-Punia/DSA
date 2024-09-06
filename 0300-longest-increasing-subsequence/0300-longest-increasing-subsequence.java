3d1f237816ee07878275a5000f983eb4cbed3d36
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-09-06 13:15:27 +0530

    Time: 438 ms (5.1%), Space: 78.2 MB (12.26%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,56 +1,21 @@
 class Solution {
-    // int ans;
-    // List<Integer> arr = new ArrayList<>();
-    // Map<String, Integer> dp = new HashMap<>();
-
-    // int tmp(int[] nums, int i, int prev) {
-    //     String key = i + "," + prev;
-    //     if (i >= nums.length) {
-    //         return 0;
-    //     }
-    //     if (dp.containsKey(key))
-    //         return dp.get(key);
-    //     int include = 0,len = 0;
-    //     len = tmp(nums, i + 1, prev);
-    //     if (prev == -1 || nums[prev] < nums[i])
-    //         len = Math.max(len, 1 + tmp(nums, i + 1, i));
-    //     dp.put(key, len);
-    //     return len;
-    // }
-    int tabulation(int[] nums,int n){
-        int[][] dp = new int[n+1][n+1];
-        for(int[] r: dp)Arrays.fill(r,0);
-        for(int i = n - 1;i>=0;i--){
-            for(int prev = i - 1;prev>=-1;prev--){
-                dp[i][prev+1] = dp[i+1][prev+1];
-                if(prev == -1 || nums[prev] < nums[i]){
-                    dp[i][prev + 1] = Math.max(dp[i][prev + 1] ,1 + dp[i+1][i+1]);
-                }
-            }
-        }
-        return dp[0][0];
-    }
-    int tabulation2(int[] nums,int n){
-        int[] ahead = new int[n+1];
-        int[] prev = new int[n+1];
-        for(int i = 0;i<=n;i++)ahead[i] = 0;
-        for(int i = n - 1;i>=0;i--){
-            for(int prev_idx = i - 1;prev_idx>=-1;prev_idx--){
-                prev[prev_idx+1] = ahead[prev_idx+1];
-                if(prev_idx == -1 || nums[prev_idx] < nums[i]){
-                    prev[prev_idx + 1] = Math.max(prev[prev_idx + 1] ,1 + ahead[i+1]);
-                }
-            }
-            for(int j = 0;j<=n;j++)ahead[j] = prev[j];
-        }
-        return ahead[0];
+    int[][] dp;
+    int ans;
+    int tmp(int[] nums,int i,int prev){
+        int newPrev = prev + 1;
+        if(i >= nums.length)
+            return 0;
+        if(dp[i][newPrev] != -1)return dp[i][newPrev];
+        int include = 0;
+        if(prev == -1 || nums[i] > nums[prev])
+            include =  1 + tmp(nums,i+1,i);
+        int exclude = tmp(nums,i+1,prev);
+        return dp[i][newPrev] = Math.max(include , exclude);
     }
     public int lengthOfLIS(int[] nums) {
-        // ans = 0;
-        // dp.clear();
-        // arr.clear();
-        // int[] dp = new int[nums.length + 1];
-        return tabulation2(nums,nums.length);
-        
+        dp = new int[nums.length][nums.length];
+        for(int[] row : dp)
+            Arrays.fill(row,-1);
+        return tmp(nums,0,-1);
     }
 }
