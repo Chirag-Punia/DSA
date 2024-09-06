efb9324fbc6dafac30334d1caecbbbb41d88e2e9
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-09-06 13:46:13 +0530

    Time: 139 ms (20%), Space: 120.9 MB (5.45%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,21 +1,40 @@
 class Solution {
     int[][] dp;
     int ans;
-    int tmp(int[] nums,int i,int prev){
+
+    int tmp(int[] nums, int i, int prev) {
         int newPrev = prev + 1;
-        if(i >= nums.length)
+        if (i >= nums.length)
             return 0;
-        if(dp[i][newPrev] != -1)return dp[i][newPrev];
+        if (dp[i][newPrev] != -1)
+            return dp[i][newPrev];
         int include = 0;
-        if(prev == -1 || nums[i] > nums[prev])
-            include =  1 + tmp(nums,i+1,i);
-        int exclude = tmp(nums,i+1,prev);
-        return dp[i][newPrev] = Math.max(include , exclude);
+        if (prev == -1 || nums[i] > nums[prev])
+            include = 1 + tmp(nums, i + 1, i);
+        int exclude = tmp(nums, i + 1, prev);
+        return dp[i][newPrev] = Math.max(include, exclude);
     }
+
+    int tabulation(int[] nums) {
+        int n = nums.length;
+        int[][] memo = new int[n+1][n+1];
+        for (int[] r : memo)
+            Arrays.fill(r, 0);
+        for(int i = n-1;i>=0;i--){
+            for(int prev = i - 1;prev >= -1;prev--){
+                memo[i][prev + 1] = memo[i+1][prev+1];
+                if(prev == -1 || nums[i] > nums[prev]){
+                    memo[i][prev+1] = Math.max(1 + memo[i+1][i+1],memo[i][prev+1]);
+                }
+            }
+        }
+        return memo[0][0];
+    }
+
     public int lengthOfLIS(int[] nums) {
         dp = new int[nums.length][nums.length];
-        for(int[] row : dp)
-            Arrays.fill(row,-1);
-        return tmp(nums,0,-1);
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+        return tabulation(nums);
     }
 }
