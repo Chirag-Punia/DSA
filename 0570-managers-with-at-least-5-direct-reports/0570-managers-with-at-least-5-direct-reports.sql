78fbd9e6197721aa4e5309c2ecd4fd2cf6323306
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-09-09 14:23:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 
-select COALESCE(e2.name, 'NULL') as name
+select e2.name
 from Employee E1
 left join Employee E2
 on E1.managerId = E2.id
-group by e2.id
+group by e2.id,e2.name
 having count(e1.id) >= 5;
