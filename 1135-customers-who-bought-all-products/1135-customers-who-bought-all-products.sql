a5cbd8c6437781da2a38ba9735a0338f9bccf22d
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-09-10 13:48:28 +0530

    Time: 584 ms (44.64%), Space: 0B (100%) - LeetHub

--- 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
-select tmp.customer_id
-from
-(select c.customer_id,count(distinct c.product_key) as cnt
-from Customer c
-group by c.customer_id) tmp
-where tmp.cnt = (select count(p.product_key) from Product p);
+
+select tmp.customer_id from
+(select customer_id,count(distinct product_key) as cnt
+from Customer 
+group by customer_id) tmp
+where tmp.cnt = (select count(product_key) from Product);
