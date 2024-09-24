0188e96ee6b25ac811766d598d00b2f06d572b68
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-09-24 20:14:40 +0530

    Time: 33 ms (16.32%), Space: 44.5 MB (18.03%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,50 +1,24 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int n = s.length();
-        int ans = 0;
-        
-        // for (int i = 0; i < n; i++) {
-        //     HashMap<Character, Integer> mpp = new HashMap<>();
-        //     int maxFreq = 0;
-            
-        //     for (int j = i; j < n; j++) {
-        //         char curr = s.charAt(j);
-        //         mpp.put(curr, mpp.getOrDefault(curr, 0) + 1);
-        //         maxFreq = Math.max(maxFreq, mpp.get(curr));
-                
-        //         int change = (j - i + 1) - maxFreq;
-        //         if (change <= k) {
-        //             ans = Math.max(ans, j - i + 1);
-        //         } else {
-        //             break;
-        //         }
-        //     }
-        // }
-        HashMap<Character, Integer> mpp = new HashMap<>();
+        HashMap<Character,Integer> mpp = new HashMap<>();
         int left = 0;
-        int maxFreq = 0; 
-        for (int right = 0; right < n; right++) {
+        int right = 0;
+        int ans = Integer.MIN_VALUE,maxf = Integer.MIN_VALUE;
+        int n = s.length();
+        while(right < n){
             char curr = s.charAt(right);
-            mpp.put(curr, mpp.getOrDefault(curr, 0) + 1);
-            maxFreq = Math.max(maxFreq, mpp.get(curr));
-
-
-            int change = (right - left + 1) - maxFreq;
-
-
-            if (change > k) {
-                char leftChar = s.charAt(left);
-                mpp.put(leftChar, mpp.get(leftChar) - 1);
-                if (mpp.get(leftChar) == 0) {
-                    mpp.remove(leftChar);
-                }
+            mpp.put(curr,mpp.getOrDefault(curr,0) + 1);
+            maxf = Math.max(maxf,mpp.get(curr));
+            int changes = (right - left + 1) - maxf;
+            if(changes > k){
+                char c = s.charAt(left);
+                mpp.put(c,mpp.get(c) - 1);
                 left++;
+                if(mpp.get(c) == 0)mpp.remove(c);
             }
-
-
-            ans = Math.max(ans, right - left + 1);
+            ans = Math.max(right - left + 1,ans);
+            right++;     
         }
-
         return ans;
     }
 }
