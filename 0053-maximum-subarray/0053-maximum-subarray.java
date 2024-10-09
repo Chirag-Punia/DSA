33bb47711aa9ab40a2241526b4f832ad37e90f1f
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-10-09 18:54:37 +0530

    Time: 1 ms (99.44%), Space: 56.9 MB (69.4%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,49 +1,12 @@
 class Solution {
-
-
-    public static int temp(int[] nums,int i,int sum,int maxSum){
-        int ans = maxSum;
-        if(i >= nums.length){
-            return ans;
-        }
-        for(int j = i;j<nums.length;j++){
-            sum = sum + nums[j];
-            if(sum > maxSum){
-            maxSum = sum;
-        }
-        }
-        
-        return temp(nums,++i,0,maxSum);
-        
-    }
     public int maxSubArray(int[] nums) {
+        int n = nums.length;
         int sum = 0;
-        int maxi = Integer.MIN_VALUE;
-        for(int i = 0; i< nums.length;i++){
-            sum = sum + nums[i];
-            if (sum > maxi){
-                maxi = sum;
-            }
-            if(sum < 0){
-                sum = 0;
-            }
+        int ans = Integer.MIN_VALUE;
+        for(int i = 0;i<n;i++){
+            if(sum < 0)sum = 0;
+            ans = Math.max(ans,sum+=nums[i]);
         }
-        return maxi;
-
-
-
-
-        // int sum = 0;
-        // int maxSum = Integer.MIN_VALUE;
-        // for(int i = 0;i<nums.length;i++){
-        //     sum = 0;
-        //     for(int j = i ; j <nums.length;j++){
-        //         sum = sum + nums[j];
-        //         if(sum > maxSum){
-        //             maxSum = sum;
-        //         }
-        //     }
-        // }
-        // return maxSum;
+        return ans;
     }
 }
