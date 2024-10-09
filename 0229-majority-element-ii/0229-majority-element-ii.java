eeeebb4e318246c30feb43a4c700b0caaa8a600d
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-10-09 19:22:06 +0530

    Time: 3 ms (74.19%), Space: 47.8 MB (59.9%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,19 +1,23 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        int ele1 = Integer.MAX_VALUE;
+        int ele2 = Integer.MAX_VALUE;
+        int cnt1 = 0;
+        int cnt2 = 0;
         int n = nums.length;
-        int cnt1 = 0, cnt2 = 0, ele1 = Integer.MAX_VALUE, ele2 = Integer.MAX_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            if (cnt1 == 0 && nums[i] != ele2) {
+        for(int i = 0;i<nums.length;i++){
+            int c = nums[i];
+            if(cnt1 == 0 && nums[i] != ele2){
                 cnt1++;
                 ele1 = nums[i];
-            } else if (cnt2 == 0 && nums[i] != ele1) {
+            }
+            else if(cnt2 == 0 && nums[i] != ele1){
                 cnt2++;
                 ele2 = nums[i];
-            } else if (nums[i] == ele1)
-                cnt1++;
-            else if (nums[i] == ele2)
-                cnt2++;
-            else {
+            }
+            else if(c == ele1)cnt1++;
+            else if(c == ele2)cnt2++;
+            else{
                 cnt1--;
                 cnt2--;
             }
