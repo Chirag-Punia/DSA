5d5183606b1fd0b2af8f5b302ef019f0abda324c
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-10-11 18:46:08 +0530

    Time: 56 ms (40.94%), Space: 50.7 MB (38.01%) - LeetHub

--- 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.java
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.java
@@ -1,20 +1,53 @@
+import java.util.*;
+
+class Pair<U, V> {
+    public final U first;
+    public final V second;
+
+    public Pair(U first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+
 class Solution {
     public int smallestChair(int[][] times, int targetFriend) {
-        int targetFriendArrivalTime = times[targetFriend][0];
-        int[] chair = new int[times.length];
-        Arrays.sort(times,Comparator.comparingInt(o -> o[0]));
+        int n = times.length;
 
-        for(int i = 0;i < times.length;i++){
-            int arrival = times[i][0];
-            int dep = times[i][1];
-            for(int j = 0;j<times.length;j++){
-                if(chair[j] <= arrival){
-                    chair[j] = dep;
-                    if(arrival == targetFriendArrivalTime)return j;
-                    break;
-                }
-            }
+        int[][] dp = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = i;
+            dp[i][1] = times[i][0];
+            dp[i][2] = times[i][1];
         }
+
+        Arrays.sort(dp, Comparator.comparingInt(o -> o[1]));
+
+        PriorityQueue<Integer> availableChairs = new PriorityQueue<>();
+        PriorityQueue<Pair<Integer, Integer>> occupiedChairs = new PriorityQueue<>(Comparator.comparing(p -> p.first));
+
+        for (int i = 0; i < n; i++) {
+            availableChairs.add(i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int friend = dp[i][0];
+            int arrivalTime = dp[i][1];
+            int leaveTime = dp[i][2];
+
+            while (!occupiedChairs.isEmpty() && occupiedChairs.peek().first <= arrivalTime) {
+                availableChairs.add(occupiedChairs.poll().second);
+            }
+
+            int chair = availableChairs.poll();
+
+            if (friend == targetFriend) {
+                return chair;
+            }
+
+            occupiedChairs.add(new Pair<>(leaveTime, chair));
+        }
+
         return -1;
     }
 }
