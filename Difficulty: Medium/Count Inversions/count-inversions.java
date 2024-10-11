3228557eabc02972b70ccded915c6a7473705e66
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-10-11 20:01:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Inversions/count-inversions.java
+++ Difficulty: Medium/Count Inversions/count-inversions.java
@@ -6,24 +6,19 @@ import java.lang.*;
 import java.util.*;
 
 class Sorting {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long t = sc.nextLong();
-
-        while (t-- > 0) {
-            long n = sc.nextLong();
-            long arr[] = new long[(int)n];
-
-            for (long i = 0; i < n; i++) arr[(int)i] = sc.nextLong();
-
-            System.out.println(new Solution().inversionCount(arr, (int)n));
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long t = Integer.parseInt(br.readLine());
+        for (int g = 0; g < t; g++) {
+            String[] str = (br.readLine()).trim().split(" ");
+            long arr[] = new long[str.length];
+            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
+            System.out.println(new Solution().inversionCount(arr));
         }
     }
 }
 
 // } Driver Code Ends
-
-
 
 
 // User function Template for Java
@@ -32,56 +27,44 @@ class Solution {
     // arr[]: Input Array
     // N : Size of the Array arr[]
     // Function to count inversions in the array.
-    static long inversionCount(long[] arr, int n) {
-        // Your Code Here
-        return mergeSort(arr, 0, n-1);
+    static long mergeSort(long arr[] , int i ,int j){
+        if(i >= j)return 0;
+        int mid = (i+j)/2;
+        long ans1 = mergeSort(arr,i,mid);
+        long ans2 = mergeSort(arr,mid + 1,j);
+        return ans1 + ans2 + merge(arr,i,j,mid);
     }
-
-    private static long mergeSort(long[] arr, int s, int e) {
-        if (s >= e) return 0;
-        int mid = (s+e)/2;
-        long ans1 = mergeSort(arr, s, mid);
-        long ans2 = mergeSort(arr, mid+1, e);
-        return ans1 + ans2 + mergeArr(arr, s, e);
-    }
-
-    private static long mergeArr(long[] arr, int s, int e) {
-        long total = 0;
-
-        int mid = (s+e)/2;
-        ArrayList<Long> list1 = new ArrayList<>();
-        ArrayList<Long> list2 = new ArrayList<>();
-        for (int i = s; i <= mid; i++) {
-            list1.add(arr[i]);
-        }
-        for (int i = mid+1; i <= e; i++) {
-            list2.add(arr[i]);
-        }
-        int i = s;
-        int j = 0;
-        int k = 0;
-        while (j < list1.size() && k < list2.size()) {
-            if (list1.get(j) <= list2.get(k)) {
-                arr[i] = list1.get(j);
-                j++;
+    static long merge(long[] arr,int i,int j,int mid){
+        List<Long> temp = new ArrayList<>();
+        int total = 0;
+        int left = i;
+        int right = mid + 1;
+        while(left <= mid && right <= j){
+            if(arr[right] >= arr[left]){
+                temp.add(arr[left]);
+                left++;
             }
-            else {
-                total += list1.size()-j;
-                arr[i] = list2.get(k);
-                k++;
+            else{
+                temp.add(arr[right]);
+                right++;
+                total += (mid - left + 1);
             }
-            i++;
         }
-        while (j < list1.size()) {
-            arr[i] = list1.get(j);
-            j++;
-            i++;
+         while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
         }
-        while (k < list2.size()) {
-            arr[i] = list2.get(k);
-            k++;
-            i++;
+        while (right <= j) {
+            temp.add(arr[right]);
+            right++;
+        }
+        for (int k = i; k <= j; k++) {
+            arr[k] = temp.get(k - i);
         }
         return total;
+    }
+    static long inversionCount(long arr[]) {
+        // Your Code Here
+        return mergeSort(arr,0,arr.length - 1);
     }
 }
