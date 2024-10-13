b2e2e851a2608e1f920bcf0e0ce0fa635c12755d
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-10-13 16:45:11 +0530

    Time: 51 ms (89.1%), Space: 135 MB (48.08%) - LeetHub

--- 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.java
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.java
@@ -1,24 +1,28 @@
-class Solution {
-    Integer dp[][];
+import java.util.Arrays;
 
-    public int maxValue(int[][] events, int k) {
-        int n = events.length;
-        dp = new Integer[n][k + 1];
-        Arrays.sort(events, (a, b) -> Integer.compare(a[0], b[0]));
-        return solve(events, k, 0, -1);
+class Solution {
+    int[][] dp;
+    int tmp(int[][] events, int i, int k) {
+        if (k == 0 || i == events.length) {
+            return 0;
+        }
+        if(dp[i][k] != -1)return dp[i][k];
+        int skip = tmp(events, i + 1, k);
+        
+        int j = i + 1;
+        for(;j < events.length;j++){
+            if(events[j][0] > events[i][1])break;
+        }
+
+        int take = events[i][2] + tmp(events, j, k - 1);
+
+        return dp[i][k] = Math.max(take, skip);
     }
 
-    public int solve(int[][] events, int k, int curr, int prev) {
-        if (curr == events.length || k == 0)
-            return 0;
-        if (prev == -1 || events[prev][1] < events[curr][0]) {
-            
-            if (dp[curr][k] != null)
-                return dp[curr][k];
-
-            return dp[curr][k] = Math.max(solve(events, k - 1, curr + 1, curr) + events[curr][2],
-                    solve(events, k, curr + 1, prev));
-        } else
-            return solve(events, k, curr + 1, prev);
+    public int maxValue(int[][] events, int k) {
+        dp = new int[events.length+1][k+1];
+        for(int[] row : dp)Arrays.fill(row,-1);
+        Arrays.sort(events, (a, b) -> Integer.compare(a[0], b[0]));
+        return tmp(events, 0, k);
     }
 }
