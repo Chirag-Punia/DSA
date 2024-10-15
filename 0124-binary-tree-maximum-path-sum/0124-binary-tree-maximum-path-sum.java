e6bcff19412bdf930d1a36586f5810a8e5067556
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-10-15 21:06:35 +0530

    Time: 0 ms (100%), Space: 44.5 MB (28.1%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -14,27 +14,19 @@
  * }
  */
 class Solution {
-    public static int ans;
-    Solution(){
-        this.ans = Integer.MIN_VALUE;
-    }
-    public static int sum(TreeNode root){
-        if(root == null){
-            return 0;
-        }
-        int l = sum(root.left);
-        int r = sum(root.right);
-        if(l<0){
-            l = 0;
-        }
-        if(r < 0){
-            r = 0;
-        }
-        ans = Math.max(ans,l+r+root.val);
-        return (root.val + Math.max(l,r));
+    int ans;
+    int sum(TreeNode root){
+        if(root == null)return 0;
+        int left = sum(root.left);
+        int right = sum(root.right);
+        left = left<0?0:left;
+        right = right<0?0:right;
+        ans = Math.max(ans,root.val+left+right);
+        return root.val + Math.max(left,right);
     }
     public int maxPathSum(TreeNode root) {
+        ans = Integer.MIN_VALUE;
         sum(root);
         return ans;
-    }  
+    }
 }
