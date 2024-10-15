33dc023b71788d0c248530f52a3dbc7963341f3b
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-10-15 13:29:49 +0530

    Time: 0 ms (100%), Space: 41.9 MB (97.04%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,42 +1,19 @@
 class Solution {
-    public static int binarySearch(int[] arr, int left, int right) {
-
-        int mid = (right + left) / 2;
-        if (left <= right) {
-            if (arr[mid] >= arr[mid + 1] && arr[mid - 1] <= arr[mid]) {
-                return mid;
-            } else if (arr[mid] < arr[mid + 1] && arr[mid - 1] < arr[mid]) {
-                return binarySearch(arr, mid + 1, right);
-            } else if (arr[mid - 1] > arr[mid] && arr[mid + 1] < arr[mid]) {
-                return binarySearch(arr, left, mid - 1);
-            }
-            //FUNNY KISSI BHI TARAF BHEJDO
-            else{
-                return binarySearch(arr, left, mid - 1);
-            }
+    int binary(int[] nums, int low, int high) {
+        if (low == high) {
+            return low;
         }
-        return mid;
+        
+        int mid = (high + low) / 2;
+        
+        if (nums[mid] > nums[mid + 1]) {
+            return binary(nums, low, mid);
+        } else {
+            return binary(nums, mid + 1, high);
+        }
     }
 
-    public int findPeakElement(int[] arr) {
-        if (arr.length > 1) {
-            if (arr[0] > arr[1]) {
-                return 0;
-            } else if (arr[arr.length - 1] > arr[arr.length - 2]) {
-                return arr.length - 1;
-            }
-        } else {
-            return 0;
-        }
-
-        return binarySearch(arr, 1, arr.length - 2);
-        // BRUTE FORCE
-        // for (int i = 1; i < arr.length - 1; i++) {
-        // if (arr[i] > arr[i + 1] && arr[i] > arr[i - 1]) {
-        // return i;
-        // }
-        // }
-
-        // return 0;
+    public int findPeakElement(int[] nums) {
+        return binary(nums, 0, nums.length - 1);
     }
 }
