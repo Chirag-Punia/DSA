e89c60581ceb0dfc8be3a81c60fe75acd5535031
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-10-16 16:43:27 +0530

    Time: 21 ms (55.22%), Space: 45.5 MB (11.17%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,54 +1,38 @@
 class Solution {
+    int temp;
+    public int temp(int[] piles,int j){
+            int n = piles.length;
 
-    public static int binarySearch(int[] piles, int h, int left, int right,int ans) {
-        int mid = (left + right) / 2;
-        int xx = ans;
-        if (left <= right) {
-            double totalFinishTime = totalFinishTime(mid, piles);
-            if (totalFinishTime <= h) {
-                return binarySearch(piles, h, left, mid - 1,Math.min(mid,xx));
-            } else {
-                return binarySearch(piles, h, mid + 1, right,xx);
+            for(int i = 0;i<n;i++){
+                double t = (double)piles[i]/j;
+                int time = (int) Math.ceil(t);
+                if(temp - time >= 0)
+                    temp -= time;
+                else{
+                    return -1;
+                }
             }
-        }
-        return xx;
+        return j;
     }
-
-    public static double totalFinishTime(int mid,int[] piles){
-        double totalFinishTime = 0;
-        for (int j = 0; j < piles.length; j++) {
-                double hours = (double)Math.ceil((double)(piles[j])/ (double)mid);
-                totalFinishTime = totalFinishTime + hours;
-            }
-        return totalFinishTime;
-    }
-
     public int minEatingSpeed(int[] piles, int h) {
-
+        int n = piles.length; 
         int max = Integer.MIN_VALUE;
-        for (int i = 0; i < piles.length; i++) {
-            max = Math.max(piles[i], max);
+        int low = 1;
+        int min = Integer.MAX_VALUE;
+        for(int i = 0;i<n;i++)
+            max = Math.max(max,piles[i]);
+        int high = max;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            temp = h;
+            int ans = temp(piles,mid);
+            if(ans != -1){
+                min = mid;
+                high = mid - 1;
+            }
+            else
+                low = mid + 1;
         }
-
-        return binarySearch(piles, h, 1, max,Integer.MAX_VALUE);
-
-        // BRUTE FORCE
-        // double totalFinishTime = 0;
-        // int n = piles.length;
-        // int max = Integer.MIN_VALUE;
-        // for (int i = 0; i < n; i++) {
-        // max = Math.max(piles[i], max);
-        // }
-        // for (int i = 1; i <= max; i++) {
-        // for (int j = 0; j < n; j++) {
-        // double hours = (double) Math.ceil((double) (piles[j]) / (double) i);
-        // totalFinishTime = totalFinishTime + hours;
-        // }
-        // if (totalFinishTime <= h) {
-        // return i;
-        // }
-        // totalFinishTime = 0;
-        // }
-        // return -1;
+        return min;
     }
 }
