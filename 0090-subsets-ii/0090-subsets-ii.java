fd558fe233adc24960458c6d376ef0cc4516f5ca
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-12-23 12:44:40 +0530

    Time: 1 ms (99.82%), Space: 43.5 MB (62.32%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,20 +1,39 @@
+// class Solution {
+//     public void temp(int[] nums, int i, Set<List<Integer>> ans, List<Integer> help) {
+//         if (i >= nums.length) {
+//             ans.add(new ArrayList<>(help));
+//             return;
+//         }
+//         help.add(nums[i]);
+//         temp(nums, i + 1, ans, help);
+//         help.remove(help.size() - 1);
+//         temp(nums, i + 1, ans, help);
+//     }
+
+//     public List<List<Integer>> subsetsWithDup(int[] nums) {
+//         Arrays.sort(nums);
+//         Set<List<Integer>> ans = new HashSet<>();
+//         temp(nums, 0, ans, new ArrayList<>());
+//         return new ArrayList<>(ans);
+//     }
+// }
 class Solution {
-    public List<List<Integer>> ans;
-    public List<Integer> arr;
-    public void tmp(int i,int[] nums){
-        ans.add(new ArrayList<>(arr));
-        for(int j = i;j<nums.length;j++){
-            if(j>i && nums[j-1] == nums[j]) continue;
-            arr.add(nums[j]);
-            tmp(j+1,nums);
-            arr.remove(arr.size() - 1);
-        }        
+    public void backtrack(int[] nums, int index, List<List<Integer>> ans, List<Integer> current) {
+        ans.add(new ArrayList<>(current)); 
+        for (int i = index; i < nums.length; i++) {
+            if (i > index && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            current.add(nums[i]); 
+            backtrack(nums, i + 1, ans, current);
+            current.remove(current.size() - 1); 
+        }
     }
+
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        ans = new ArrayList<>();
-        arr = new ArrayList<>();
-        Arrays.sort(nums);
-        tmp(0,nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums); 
+        backtrack(nums, 0, ans, new ArrayList<>());
         return ans;
     }
 }
