865f3d2d8314830ad364455512e25f488faff17d
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-12-24 17:07:28 +0530

    Time: 0 ms (100%), Space: 42.2 MB (44.39%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,49 +14,15 @@
  * }
  */
 class Solution {
-    public static class nodeInfo{
-        TreeNode node;
-        int row;
-        nodeInfo(TreeNode node,int row){
-            this.node = node;
-            this.row = row;
-        }
-    }
-    public static void right(TreeNode root,int level,List<Integer> arr){
-        if(root == null) return;
-        if(level == arr.size()) arr.add(root.val);
-        right(root.right,level + 1,arr);
-        right(root.left,level + 1,arr);
+    void temp(TreeNode root,int level,List<Integer> ans){
+        if(root == null)return;
+        if(level == ans.size())ans.add(root.val);
+        temp(root.right,level+1,ans);
+        temp(root.left,level+1,ans);
     }
     public List<Integer> rightSideView(TreeNode root) {
-
-        List<Integer> arr = new ArrayList<>();
-        right(root,0,arr);
-        return arr;
-
-        //BRUTE FORCE
-        // Queue<nodeInfo> q = new LinkedList<>();
-        // TreeMap<Integer,List<Integer>> mpp = new TreeMap<>();
-        // List<Integer> ans = new ArrayList<>();
-        // if(root == null){
-        //     return ans;
-        // }
-        // q.offer(new nodeInfo(root,0));
-        // while(!q.isEmpty()){
-        //     nodeInfo c = q.poll();
-        //     mpp.computeIfAbsent(c.row,k -> new ArrayList<>()).add(c.node.val);
-        //     if(c.node.left != null){
-        //         q.offer(new nodeInfo(c.node.left,c.row + 1));
-        //     }
-        //     if(c.node.right != null){
-        //         q.offer(new nodeInfo(c.node.right,c.row + 1));
-        //     }
-        // }
-        
-        // for(Map.Entry<Integer,List<Integer>> entry : mpp.entrySet()){
-        //     List<Integer> x = entry.getValue();
-        //     ans.add(x.get(x.size() - 1));
-        // }
-        // return ans;
+        List<Integer> ans = new ArrayList<>();
+        temp(root,0,ans);
+        return ans;
     }
 }
