ea2dc9777cbef6c958122f08a06fefbea98f6ef6
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-12-25 23:18:33 +0530

    Time: 2 ms (76.97%), Space: 61.9 MB (34.11%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,26 +1,10 @@
 class Solution {
-    public int maxProfit(int[] arr) {
-        int n = arr.length;
-        int min = Integer.MAX_VALUE;
-        int maxPRO = 0;
-        for(int i = 0;i<n;i++){
-            min = Math.min(min,arr[i]);
-            maxPRO = Math.max(maxPRO,arr[i] - min);
+    public int maxProfit(int[] prices) {
+        int mini = Integer.MAX_VALUE,maxi = Integer.MIN_VALUE;
+        for(int i = 0;i<prices.length;i++){
+            mini = Math.min(mini,prices[i]);
+            maxi = Math.max(maxi,prices[i] - mini);
         }
-        return maxPRO;
-        
-
-    //BRUTE FORCE
-        // int ans = 0;
-        // for(int i = 0; i<n-1;i++){
-        //     for(int j = i+1;j<n;j++){
-        //         int diff = prices[j] - prices[i];
-        //         ans = Math.max(ans,diff);
-        //     }
-        // }
-        // return ans;
+        return maxi;
     }
-
-
-
 }
