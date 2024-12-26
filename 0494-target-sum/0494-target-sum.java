96dd84fa26e431098465e3a516967ba751866b30
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-12-26 22:48:56 +0530

    Time: 7 ms (74.15%), Space: 45.1 MB (21.68%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,22 +1,21 @@
 class Solution {
-    public HashMap<String,Integer> dp;
-
-    public int tmp(int[] nums, int target, int i, int currSum) {
-        String key = i+","+currSum;
-        if (dp.containsKey(key))
-            return dp.get(key);
-        if (i < 0 && target == currSum)
-            return 1;
-        if (i < 0)
-            return 0;
-        int pos = tmp(nums, target, i - 1, currSum + nums[i]);
-        int neg = tmp(nums, target, i - 1, currSum - nums[i]);
-        dp.put(key,pos+neg);
-        return dp.get(key);
+    int helper(int[] nums,int t,int i,int curr,int[][] dp,int offset){
+        if(i >= nums.length){
+            return curr == t ? 1 : 0;
+        }
+        if(dp[i][curr + offset] != -1)return dp[i][curr+offset];
+        int pos = helper(nums,t,i+1,curr + nums[i],dp,offset);
+        int neg = helper(nums,t,i+1,curr - nums[i],dp,offset);
+        return dp[i][curr + offset] = pos+neg;
     }
-
     public int findTargetSumWays(int[] nums, int target) {
-        dp = new HashMap<>();
-        return tmp(nums, target, nums.length - 1, 0);
+        int n = nums.length;
+        int sum = 0;
+        for(int a : nums)sum+=a;
+        if(Math.abs(target) > sum)return 0;
+        int offset = sum;
+        int[][] dp = new int[n+1][2*sum + 1];
+        for(int[] row : dp)Arrays.fill(row,-1);
+        return helper(nums,target,0,0,dp,offset);
     }
 }
