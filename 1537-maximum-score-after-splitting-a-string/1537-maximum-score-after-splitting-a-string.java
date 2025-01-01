b35885f68e94de1c2096cfa6f46207dc77bd6897
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2025-01-01 21:39:44 +0530

    Time: 1 ms (97.36%), Space: 41.7 MB (36.08%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
@@ -1,27 +1,23 @@
 class Solution {
-    int count0(String s){
-        int cnt = 0;
-        for(char a : s.toCharArray()){
-            if(a == '0')cnt++;
-        }
-        return cnt;
-    }
-    int count1(String s){
-        int cnt = 0;
-        for(char a : s.toCharArray()){
-            if(a == '1')cnt++;
-        }
-        return cnt;
-    }
     public int maxScore(String s) {
-        int maxi = Integer.MIN_VALUE;
         int n = s.length();
-        for(int i = 0;i<n;i++){
-            String temp1 = s.substring(0,i+1);
-            String temp2 = s.substring(i+1);
-            if(!temp1.equals("") && !temp2.equals(""))
-                maxi = Math.max(maxi,count0(temp1) + count1(temp2));
+        int ones = 0; 
+
+        for (int i = 0; i < n; i++)
+            if (s.charAt(i) == '1')
+                ones++;
+
+        int zeros = 0; 
+        int maxScore = 0;
+
+        for (int i = 0; i < n - 1; i++) { 
+            if (s.charAt(i) == '0') 
+                zeros++;
+            else
+                ones--;
+            maxScore = Math.max(maxScore, zeros + ones);
         }
-        return maxi;
+
+        return maxScore;
     }
 }
